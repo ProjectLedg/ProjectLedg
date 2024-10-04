316294f5ec2b4979dcdbf4ff7607d72f06d71b55
ChasAlex <alexander.lundquist@chasacademy.se>
2024-10-04 13:57:39 +0200

    Added some structure to the pages with stumps and added react-router-dom

--- projectledg.client/src/App.jsx
+++ projectledg.client/src/App.jsx
@@ -1,12 +1,32 @@
 import './App.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import LandingPage from './pages/LandingPage';
+import LandingPage from './pages/LandingPages/LandingPage';
+import LoginPage from './pages/AccountHandlingPages/LoginPage';
+import SignupPage from './pages/AccountHandlingPages/SignupPage';
+import CompanySelectPage from './pages/AccountHandlingPages/CompanySelectPage';
+import DashboardPage from './pages/DashboardPages/DashboardPage';
+import WhyPage from './pages/LandingPages/WhyPage';
+import FeaturePage from './pages/LandingPages/FeaturePage';
+import PricingPage from './pages/LandingPages/PricingPage';
+import ContactPage from './pages/LandingPages/ContactPage';
+import CompanyCreatePage from './pages/AccountHandlingPages/CompanyCreatePage';
 
 function App() {
     return (
         <Router>
             <Routes>
                 <Route path="/" element={<LandingPage />} />
+                <Route path="/login" element={<LoginPage/>}/>
+                <Route path="/signup" element={<SignupPage/>}/>
+                <Route path="/company-select" element={<CompanySelectPage/>}/>
+                <Route path="/dashboard" element={<DashboardPage/>}/>
+                <Route path="/why" element={<WhyPage/>}/>
+                <Route path="/feature" element={<FeaturePage/>}/>
+                <Route path="/pricing" element={<PricingPage/>}/>
+                <Route path="/contact" element={<ContactPage/>}/>
+                <Route path="/company-create" element={<CompanyCreatePage/>}/>
+
+                
             </Routes>
         </Router>
     );
