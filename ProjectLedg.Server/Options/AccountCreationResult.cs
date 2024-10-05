967d1a80f561881df0de1024844b17330ea94ce7
manhattaa <fadynhatta@gmail.com>
2024-10-05 14:34:44 +0200

    Added an AccountCreation Result that will feed us the right response messages upon a successful account creation.

---
+++ ProjectLedg.Server/Options/AccountCreationResult.cs
@@ -0,0 +1,10 @@
+﻿namespace ProjectLedg.Server.Options
+{
+    public class AccountCreationResult
+    {
+        public bool Success { get; set; }
+        public string? Token { get; set; }
+        //To support multiple errorMessages
+        public IEnumerable<string>? Errors { get; set; }
+    }
+}
