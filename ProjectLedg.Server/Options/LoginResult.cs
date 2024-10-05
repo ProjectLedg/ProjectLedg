bdbaa4831b19339dbe9c2f6015c035b7cbfc0e60
manhattaa <fadynhatta@gmail.com>
2024-10-05 14:35:14 +0200

    Created a LoginResult that will feed us information upon a successful/unsuccessful login

---
+++ ProjectLedg.Server/Options/LoginResult.cs
@@ -0,0 +1,15 @@
+﻿namespace ProjectLedg.Server.Options
+{
+    public class LoginResult
+    {
+        public bool Success { get; set; }
+        public string Token { get; set; }
+        public string ErrorMessage { get; set; }
+        public bool Require2FA { get; set; }
+
+        public static LoginResult Failed(string message) => new LoginResult { Success = false, ErrorMessage = message };
+        public static LoginResult Successful(string token) => new LoginResult { Success = true, Token = token };
+
+        public static LoginResult Requires2FA(string message) => new LoginResult { Require2FA = true };
+    }
+}
