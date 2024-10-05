7d55e37fd23e18fbdbb30dd04db4b89928aab95d
ChasAlex <alexander.lundquist@chasacademy.se>
2024-10-05 15:20:52 +0200

    Set up SignUpPage.Jsx with responsive pages, Email is being passed to the CompanyCreatePage.jsx componet to be proccessed in the latter stage

--- projectledg.client/src/pages/AccountHandlingPages/CompanyCreatePage.jsx
+++ projectledg.client/src/pages/AccountHandlingPages/CompanyCreatePage.jsx
@@ -1,8 +1,15 @@
+import React from 'react'
+import { useLocation } from 'react-router-dom'
 
 export default function CompanyCreatePage() {
-    return(
-        <>
-        </>
-    )
+    const location = useLocation()
+    const email = location.state?.email
+
+     return (
+    <div>
+      <h1>Welcome!</h1>
+      <p>Your email: {email}</p>
+    </div>
+  )
 
 }
