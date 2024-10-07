e6396ff3f658b170ccfedc5bafd67e6aded57585
Adrian Rozsahegyi <122897788+Adrozs@users.noreply.github.com>
2024-10-07 12:19:53 +0200

    added company select starting code

--- projectledg.client/src/App.jsx
+++ projectledg.client/src/App.jsx
@@ -1,20 +1,22 @@
 import { useEffect, useState } from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import CompanySelectPage from "./pages/AccountHandlingPages/CompanySelectPage";
 import './App.css';
 
 function App() {
     return (
         <Router>
             <Routes>
-                <Route path="/" element={<LandingPage />} />
-                <Route path="/login" element={<LoginPage/>}/>
-                <Route path="/signup" element={<SignupPage/>}/>
+                <Route path="/" element={<CompanySelectPage />} />
+                {/*<Route path="/login" element={<LoginPage/>}/>
+                <Route path="/signup" element={<SignupPage/>}/> */}
                 <Route path="/company-select" element={<CompanySelectPage/>}/>
-                <Route path="/dashboard" element={<DashboardPage/>}/>
+                {/* <Route path="/dashboard" element={<DashboardPage/>}/>
                 <Route path="/why" element={<WhyPage/>}/>
                 <Route path="/feature" element={<FeaturePage/>}/>
                 <Route path="/pricing" element={<PricingPage/>}/>
                 <Route path="/contact" element={<ContactPage/>}/>
-                <Route path="/company-create" element={<CompanyCreatePage/>}/>             
+                <Route path="/company-create" element={<CompanyCreatePage/>}/>              */}
             </Routes>
         </Router>
     );    
--- projectledg.client/src/pages/AccountHandlingPages/CompanySelectPage.jsx
+++ projectledg.client/src/pages/AccountHandlingPages/CompanySelectPage.jsx
@@ -1,8 +1,26 @@
+import { Card, CardContent, CardHeader } from "@/components/ui/card"
+
 
 export default function CompanySelectPage() {
     return(
         <>
+            <section className="">
+                <h1>Select comapny</h1>
+                <Card className="w-40">
+                    <CardHeader>ProjectLedge AB</CardHeader>
+                    <CardContent>
+                        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aliquid, beatae?</p>
+                    </CardContent>
+                </Card>
+
+                <Card className="w-40">
+                    <CardHeader>ProjectLedge AB</CardHeader>
+                    <CardContent>
+                        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aliquid, beatae?</p>
+                    </CardContent>
+                </Card>
+            </section>
         </>
-    )
+    )   
 
 }
