6dd1bf469464861b86099a1af7cd4287835b5dc1
Hjalmar Stranninge <Hjalmar.stranninge@outlook.com>
2024-10-08 15:18:14 +0200

    Added CardShow component to the page

--- projectledg.client/src/pages/LandingPages/LandingPage.jsx
+++ projectledg.client/src/pages/LandingPages/LandingPage.jsx
@@ -1,17 +1,17 @@
-import React from 'react';
-import Navbar from './LandingPageComp/Navbar';
-import Content from './LandingPageComp/Content';
+import React from "react";
+import Navbar from "./LandingPageComp/Navbar";
+import Content from "./LandingPageComp/Content";
+import CardShow from "./LandingPageComp/CardShow";
 
 export default function LandingPage() {
-    return (
-        <>
-        <Navbar />
-        <div className='w-[100wv] flex flex-row justify-center'>
-        <Content/>
-        </div>
-        
-        </>
-    );
-};
+  return (
+    <>
+      <Navbar />
+      <div className="w-[100vw] flex flex-row justify-center">
+        <Content />
+      </div>
+        <CardShow />
 
-    
+    </>
+  );
+}
