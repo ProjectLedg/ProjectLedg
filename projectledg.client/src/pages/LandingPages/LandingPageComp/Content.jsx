ae03f53998a27537f597fc4af5fbdfc313aeb7b8
Hjalmar Stranninge <Hjalmar.stranninge@outlook.com>
2024-10-08 11:11:48 +0200

    Fixed spacing on call to action buttons on mobile

--- projectledg.client/src/pages/LandingPages/LandingPageComp/Content.jsx
+++ projectledg.client/src/pages/LandingPages/LandingPageComp/Content.jsx
@@ -1,16 +1,22 @@
-import { Button } from "@/components/ui/button"
-export default function Content() {
-    return(
-        <div className="flex flex-col w-[80vw] h-full justify-center items-center my-36">
-        <h1 className="py-10 scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">Data till insikter på minuter</h1>
-        <h3 className="scroll-m-20 text-2xl font-semibold tracking-tight">Bättre än Visma och Fortnox</h3>
-        <div className="flex pt-8 flex-row w-[20vw] justify-between ">
-            <Button className="w-32">Kom igång</Button>
-            <Button className="w-32">Läs mer</Button>
-        </div>
-        
-        
-        </div>
-    )
+import { Button } from "@/components/ui/button";
+import { useNavigate } from "react-router-dom";
 
+export default function Content() {
+  const navigate = useNavigate();
+  return (
+    <div className="flex flex-col w-[80vw] h-full justify-center items-center my-36">
+      <h1 className="py-10 scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
+        Data till insikter på minuter
+      </h1>
+      <h3 className="scroll-m-20 text-2xl font-semibold tracking-tight">
+        Bättre än Visma och Fortnox
+      </h3>
+      <div className="flex pt-8 flex-row w-[70vw] md:w-[20vw] justify-around">
+        <Button className="w-32" onClick={() => navigate("/signup")}>
+          Kom igång
+        </Button>
+        <Button className="w-32">Läs mer</Button>
+      </div>
+    </div>
+  );
 }
