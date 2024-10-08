c440aa0c829dff747451d110ba2c770cc0bb2b3a
Hjalmar Stranninge <Hjalmar.stranninge@outlook.com>
2024-10-08 13:59:15 +0200

    Added some styling and animated button

--- projectledg.client/src/App.jsx
+++ projectledg.client/src/App.jsx
@@ -15,17 +15,17 @@ function App() {
     return (
         <Router>
             <Routes>
-                <Route path="/" element={<CompanySelectPage />} />
-                {/*<Route path="/login" element={<LoginPage/>}/>
-                <Route path="/signup" element={<SignupPage/>}/> */}
+                <Route path="/" element={<LandingPage />} />
+                <Route path="/login" element={<LoginPage/>}/>
+                <Route path="/signup" element={<SignupPage/>}/> 
                 <Route path="/company-select" element={<CompanySelectPage/>}/>
-                {/* <Route path="/dashboard" element={<DashboardPage/>}/>
+                <Route path="/dashboard" element={<DashboardPage/>}/>
                 <Route path="/why" element={<WhyPage/>}/>
                 <Route path="/feature" element={<FeaturePage/>}/>
                 <Route path="/pricing" element={<PricingPage/>}/>
                 <Route path="/contact" element={<ContactPage/>}/>
-                <Route path="/company-create" element={<CompanyCreatePage/>}/>              */}
-            </Routes>
+                <Route path="/company-create" element={<CompanyCreatePage/>}/>      
+            </Routes>       
         </Router>
     );    
 }
--- projectledg.client/src/pages/LandingPages/LandingPageComp/Content.jsx
+++ projectledg.client/src/pages/LandingPages/LandingPageComp/Content.jsx
@@ -6,16 +6,31 @@ export default function Content() {
   return (
     <div className="flex flex-col w-[80vw] h-full justify-center items-center my-36">
       <h1 className="py-10 scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
-        Data till insikter på minuter
+        Data till insikter på <span className="text-green-500">minuter</span>
       </h1>
+
       <h3 className="scroll-m-20 text-2xl font-semibold tracking-tight">
         Bättre än Visma och Fortnox
       </h3>
+
       <div className="flex pt-8 flex-row w-[70vw] md:w-[20vw] justify-around">
         <Button className="w-32" onClick={() => navigate("/signup")}>
-          Kom igång
+          Läs mer
         </Button>
-        <Button className="w-32">Läs mer</Button>
+        {/*  <div className="relative flex items-center justify-center">
+          <div className="absolute inset-0 h-full w-full rounded-full animate-rotate bg-[conic-gradient(pink_20deg,transparent_120deg)] opacity-0 transition-opacity duration-200 hover:opacity-100"></div>
+          <Button className="relative z-10 w-32 bg-green-500 text-black hover:bg-white">
+            
+          </Button>
+        </div> */}
+        <button className="group relative inline-flex items-center justify-center overflow-hidden w-32  text-sm font-medium text-slate-800 transition-colors duration-700 ease-out hover:text-green-500 focus:outline-none focus:ring active:bg-green-800 active:text-white rounded-full">
+          <span className="absolute inset-0 rounded-full border-2 border-transparent transition-all duration-700 ease-out group-hover:border-green-500"></span>
+          <span className="absolute left-0 top-0 h-[2px] w-0 bg-green-500 transition-all duration-700 ease-out group-hover:w-full"></span>
+          <span className="absolute right-0 top-0 h-0 w-[2px] bg-green-500 transition-all duration-700 ease-out group-hover:h-full"></span>
+          <span className="absolute bottom-0 right-0 h-[2px] w-0 bg-green-500 transition-all duration-700 ease-out group-hover:w-full"></span>
+          <span className="absolute bottom-0 left-0 h-0 w-[2px] bg-green-500 transition-all duration-700 ease-out group-hover:h-full"></span>
+          <span className="relative z-10">Kom igång</span>
+        </button>
       </div>
     </div>
   );
