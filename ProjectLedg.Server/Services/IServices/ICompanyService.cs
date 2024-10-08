19ea3ac18b63e6ae083d023c8ac0885e21855381
Fady <145761517+Manhattaa@users.noreply.github.com>
2024-10-08 14:02:15 +0200

    Company Service Layer

---
+++ ProjectLedg.Server/Services/IServices/ICompanyService.cs
@@ -0,0 +1,19 @@
+﻿using ProjectLedg.Server.Data.Models;
+using ProjectLedg.Server.Data.Models.DTOs.Company;
+using ProjectLedg.Server.Model.DTOs.User;
+using ProjectLedg.Server.Options;
+using System.Security.Claims;
+
+namespace ProjectLedg.Server.Services.IServices
+{
+    public interface ICompanyService
+    {
+        Task<IEnumerable<Company>> GetAllCompaniesAsync();
+        Task<Company> GetCompanyByIdAsync(int id);
+        Task<CompanyDTO> CreateCompanyAsync(CreateCompanyDTO request);
+        //Task<> UpdateCompanyAsync(Company company);
+        //Task<> DeleteCompanyAsync(string password, ClaimsPrincipal currentUser);
+        //Task<> LoginAsync(string email, string password);
+        //Task<> SendEmailVerificationAsync(string userId, string code);
+    }
+}
