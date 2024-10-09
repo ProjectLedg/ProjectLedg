fa5cb7c21563bad37580140304331933684be0fb
Hjalmar Stranninge <Hjalmar.stranninge@outlook.com>
2024-10-09 14:16:32 +0200

    Swapped the original code for the GridItem component

--- projectledg.client/src/pages/LandingPages/LandingPageComp/CardShow.jsx
+++ projectledg.client/src/pages/LandingPages/LandingPageComp/CardShow.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { BadgeDollarSign } from "lucide-react";
+import GridItem from "./GridItem.jsx";
 
 export default function CardShow() {
   function getOpacity(x, y) {
@@ -36,27 +37,12 @@ export default function CardShow() {
                 const x = index % 8; // Column index
                 const y = Math.floor(index / 8); // Row index
                 return (
-                  <div
-                    key={index}
-                    className="relative bg-gray-100 rounded-lg shadow-lg shadow-black/30 aspect-square"
-                    style={{
-                      opacity: getOpacity(x, y),
-                    }}
-                  >
-                    <div className="flex flex-col justify-around items-center pt-4 h-full">
-                      <div className="flex flex-col justify-center px-5 py-1 rounded-full bg-green-300">
-                        <BadgeDollarSign color="black" size={36} />
-                      </div>
-                      <div className="paragraf flex flex-col w-full align-center items-center text-center">
-                        Jag hatar att bokföra!!!
-                      </div>
-                    </div>
-                  </div>
+                  <GridItem key={index} opacity={getOpacity(x, y)} />
                 );
               })}
           </div>
         </div>
-      </div>
+      </div>  
       <div
         className="absolute inset-0 pointer-events-none"
         style={{
