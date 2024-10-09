93c04616bdece2520c6f84e7564a8705885c1d76
Hjalmar Stranninge <Hjalmar.stranninge@outlook.com>
2024-10-09 14:45:21 +0200

    Made GridItem dynamic to take in different texts and icons

--- projectledg.client/src/pages/LandingPages/LandingPageComp/GridItem.jsx
+++ projectledg.client/src/pages/LandingPages/LandingPageComp/GridItem.jsx
@@ -1,18 +1,17 @@
 import React from "react";
-import { BadgeDollarSign } from "lucide-react";
 
-const GridItem = ({ opacity }) => {
+const GridItem = ({ opacity, text, icon: Icon, bgColor }) => {
   return (
     <div
       className="relative bg-gray-100 rounded-lg shadow-lg shadow-black/30 aspect-square"
       style={{ opacity }}
     >
       <div className="flex flex-col justify-around items-center pt-4 h-full">
-        <div className="flex flex-col justify-center px-5 py-1 rounded-full bg-green-300">
-          <BadgeDollarSign color="black" size={36} />
+        <div className={`flex flex-col justify-center px-5 py-1 rounded-full ${bgColor}`}>
+          <Icon color="black" size={36} />
         </div>
-        <div className="paragraf flex flex-col w-full align-center items-center text-center">
-          Jag hatar att bokföra!!!
+        <div className="paragraf flex flex-col text-xs w-full align-center items-center text-center p-1">
+          {text}
         </div>
       </div>
     </div>
