2b8a04b89d3e8f5d5f0b54455ccc191c585012bb
Fady <145761517+Manhattaa@users.noreply.github.com>
2024-10-10 13:35:00 +0200

    Created two methods that sends stuff to the Database; We've got GetInvoiceById and SaveInvoiceAsync

---
+++ ProjectLedg.Server/Repositories/IRepositories/IPDFRepository.cs
@@ -0,0 +1,10 @@
+﻿using ProjectLedg.Server.Data.Models;
+
+namespace ProjectLedg.Server.Repositories.IRepositories
+{
+    public interface IPDFRepository
+    {
+        Task SaveInvoiceAsync(Invoice invoice, string pdfFilePath);
+        Task<Invoice> GetInvoiceByIdAsync(int id);
+    }
+}
