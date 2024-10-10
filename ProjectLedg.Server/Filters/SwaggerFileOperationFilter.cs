a778256e6cd45523d28357d7cd204081c36490eb
manhattaa <fadynhatta@gmail.com>
2024-10-10 20:31:49 +0200

    Helper class for enabling Swagger File uploads

---
+++ ProjectLedg.Server/Filters/SwaggerFileOperationFilter.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.SwaggerGen;
+using System.Linq;
+
+namespace ProjectLedg.Server.Filters
+{
+    public class SwaggerFileOperationFilter : IOperationFilter
+    {
+        public void Apply(OpenApiOperation operation, OperationFilterContext context)
+        {
+            var fileParameters = context.MethodInfo.GetParameters()
+                .Where(p => p.ParameterType == typeof(IFormFile));
+
+            if (fileParameters.Any())
+            {
+                operation.RequestBody = new OpenApiRequestBody
+                {
+                    Content =
+                    {
+                        ["multipart/form-data"] = new OpenApiMediaType
+                        {
+                            Schema = new OpenApiSchema
+                            {
+                                Type = "object",
+                                Properties =
+                                {
+                                    ["file"] = new OpenApiSchema
+                                    {
+                                        Type = "string",
+                                        Format = "binary"
+                                    }
+                                }
+                            }
+                        }
+                    }
+                };
+            }
+        }
+    }
+}
