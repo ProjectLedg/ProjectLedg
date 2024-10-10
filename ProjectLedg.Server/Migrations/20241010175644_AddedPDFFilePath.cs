47ef9be314411918f07d10bc4740fff03ab06a44
manhattaa <fadynhatta@gmail.com>
2024-10-10 20:31:59 +0200

    Migrations for the new changes

---
+++ ProjectLedg.Server/Migrations/20241010175644_AddedPDFFilePath.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ProjectLedg.Server.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedPDFFilePath : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "InvoiceFile",
+                table: "Invoices",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(byte[]),
+                oldType: "varbinary(max)");
+
+            migrationBuilder.AddColumn<string>(
+                name: "InvoiceFilePath",
+                table: "Invoices",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "InvoiceFilePath",
+                table: "Invoices");
+
+            migrationBuilder.AlterColumn<byte[]>(
+                name: "InvoiceFile",
+                table: "Invoices",
+                type: "varbinary(max)",
+                nullable: false,
+                defaultValue: new byte[0],
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+        }
+    }
+}
