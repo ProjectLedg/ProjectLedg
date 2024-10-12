e76478c191fa793a451dbe50c642df55e266fa5b
manhattaa <fadynhatta@gmail.com>
2024-10-12 19:02:56 +0200

    Updated the Database with Migrations.

---
+++ ProjectLedg.Server/Migrations/20241012104636_NotMappingInvoiceFile.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ProjectLedg.Server.Migrations
+{
+    /// <inheritdoc />
+    public partial class NotMappingInvoiceFile : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "InvoiceFile",
+                table: "Invoices");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "InvoiceFile",
+                table: "Invoices",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+    }
+}
