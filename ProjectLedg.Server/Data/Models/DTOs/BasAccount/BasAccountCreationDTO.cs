b14c2d4e1defff6822077a5a527587c27a610af5
manhattaa <fadynhatta@gmail.com>
2024-10-13 22:28:02 +0200

    BasAccountDTOs

---
+++ ProjectLedg.Server/Data/Models/DTOs/BasAccount/BasAccountCreationDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace ProjectLedg.Server.Data.Models.DTOs.BasAccount
+{
+    public class BasAccountCreationDTO
+    {
+        public string Description { get; set; }
+        public string AccountNumber { get; set; }
+        public decimal TotalAmount { get; set; }
+    }
+}
---
+++ ProjectLedg.Server/Data/Models/DTOs/BasAccount/BasAccountUpdateDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace ProjectLedg.Server.Data.Models.DTOs.BasAccount
+{
+    public class BasAccountUpdateDTO
+    {
+        public int Id { get; set; }
+        public string Description { get; set; }
+        public string AccountNumber { get; set; }
+        public decimal TotalAmount { get; set; }
+    }
+}
