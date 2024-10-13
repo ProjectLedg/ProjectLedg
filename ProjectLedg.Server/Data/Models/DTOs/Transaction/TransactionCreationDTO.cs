11b5e7d5b7d6723d2088025109f65e4650a7d6e0
manhattaa <fadynhatta@gmail.com>
2024-10-13 22:29:42 +0200

    Transaction DTOs

---
+++ ProjectLedg.Server/Data/Models/DTOs/Transaction/TransactionCreationDTO.cs
@@ -0,0 +1,12 @@
+﻿namespace ProjectLedg.Server.Data.Models.DTOs.Transaction
+{
+    public class TransactionCreationDTO
+    {
+        public int Id { get; set; }
+        public decimal Debit { get; set; }
+        public decimal Credit { get; set; }
+        public DateTime TransactionDate { get; set; }
+
+        public int BasAccountId { get; set; }
+    }
+}
---
+++ ProjectLedg.Server/Data/Models/DTOs/Transaction/TransactionDTO.cs
@@ -0,0 +1,16 @@
+﻿using ProjectLedg.Server.Data.Models.DTOs.BasAccount;
+
+namespace ProjectLedg.Server.Data.Models.DTOs.Transaction
+{
+    public class TransactionDTO
+    {
+        public int Id { get; set; }
+        public decimal Debit { get; set; }
+        public decimal Credit { get; set; }
+        public DateTime TransactionDate { get; set; }
+        public int BasAccountId { get; set; }
+
+
+        public BasAccountDTO BasAccount { get; set; }
+    }
+}
