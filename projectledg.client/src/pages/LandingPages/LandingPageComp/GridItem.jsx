3cfc7f7d2248deeb3e5afa5acee15a146f41a801
HjalmarStranninge <hjalmar.stranninge@gmail.com>
2024-10-14 11:17:56 +0200

    Deleted unused component EmptyGridItem

--- projectledg.client/src/pages/LandingPages/LandingPageComp/GridItem.jsx
+++ projectledg.client/src/pages/LandingPages/LandingPageComp/GridItem.jsx
@@ -2,24 +2,24 @@ import { motion, AnimatePresence } from "framer-motion";
 
 const GridItem = ({ color, text, icon: IconComponent }) => {
   return (
-    
+
     <div className="relative bg-gray-100 rounded-lg shadow-lg shadow-black/30 aspect-square">
       <div className="flex flex-col justify-around items-center pt-4 h-full">
         <AnimatePresence mode="wait">
           <motion.div
-            key={text} 
-            initial={{ opacity: 0, scale: 0.8 }} 
+            key={text}
+            initial={{ opacity: 0, scale: 0.8 }}
             animate={{
               opacity: 1,
               scale: 1,
-              transition: { duration: 0.8 } 
+              transition: { duration: 0.8 }
             }}
             exit={{
               opacity: 0,
               scale: 0.8,
               y: -20,
               filter: 'blur(20px)',
-              transition: { duration: 0.5 } 
+              transition: { duration: 0.5 }
             }}
             className={`flex flex-col justify-center px-5 py-1 rounded-full ${color}`}
           >
@@ -29,18 +29,18 @@ const GridItem = ({ color, text, icon: IconComponent }) => {
 
         <AnimatePresence mode="wait">
           <motion.div
-            key={text} 
-            initial={{ opacity: 0, y: 10 }} 
+            key={text}
+            initial={{ opacity: 0, y: 10 }}
             animate={{
               opacity: 1,
               y: 0,
-              transition: { duration: 1 } 
+              transition: { duration: 1 }
             }}
             exit={{
               opacity: 0,
               y: -20,
               filter: 'blur(20px)',
-              transition: { duration: 0.5 } 
+              transition: { duration: 0.5 }
             }}
             className="paragraf flex flex-col text-xs w-full align-center items-center text-center p-1"
           >
