3558506774c36aef1d9e7daa9ff244efdacdff24
jonnytouma <jonnytouma@gmail.com>
2024-10-15 16:12:08 +0200

    Added new component to feature page

--- projectledg.client/src/pages/LandingPages/FeaturePage.jsx
+++ projectledg.client/src/pages/LandingPages/FeaturePage.jsx
@@ -1,18 +1,16 @@
 import React from "react";
 import Navbar from "./LandingPageComp/Navbar";
+import FeatureContent from "./FeaturePageComp/FeatureContent";
 
 export default function FeaturePage() {
     return (
         <>
-
             <Navbar />
             <div className="w-[100vw] flex flex-row justify-center">
-                <p>hej</p>
+                <FeatureContent />
             </div>
             <div className="w-[100vw] overflow-auto">
-
             </div>
-
         </>
     )
 }
