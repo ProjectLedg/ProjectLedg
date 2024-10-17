ab5b6c4e79abbc72a4003d16377d6147685438e6
Fady <145761517+Manhattaa@users.noreply.github.com>
2024-10-17 12:27:23 +0200

    Added the Deployed website URL to CORS

--- ProjectLedg.Server/Program.cs
+++ ProjectLedg.Server/Program.cs
@@ -112,7 +112,7 @@ namespace ProjectLedg.Server
                 options.AddDefaultPolicy(builder =>
                 {
 
-                    builder.WithOrigins("http://localhost:5173", "https://localhost:5173", "https://accounts.google.com", "https://localhost:7223", "https://localhost:7294") //7294 is a placeholder for MVC project.
+                    builder.WithOrigins("http://localhost:5173", "https://localhost:5173", "https://accounts.google.com", "https://localhost:7223", "https://localhost:7294", "https://projectledg.azurewebsites.net/")
                         .AllowAnyMethod()
                         .AllowAnyHeader()
                         .AllowCredentials();
