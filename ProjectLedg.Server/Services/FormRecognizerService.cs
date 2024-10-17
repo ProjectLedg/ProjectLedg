6d91cd2909a2a16727886d8e9b703241beef6480
manhattaa <fadynhatta@gmail.com>
2024-10-17 22:21:00 +0200

    Service Layer for the Azure Form Recognizer.

---
+++ ProjectLedg.Server/Services/FormRecognizerService.cs
@@ -0,0 +1,37 @@
+﻿using Azure.AI.FormRecognizer.DocumentAnalysis;
+using Azure;
+using Sprache;
+
+namespace ProjectLedg.Server.Services
+{
+    public class FormRecognizerService
+    {
+        private readonly DocumentAnalysisClient _client;
+        public FormRecognizerService(string endpoint, string apiKey)
+        {
+            var credential = new AzureKeyCredential(apiKey);
+            _client = new DocumentAnalysisClient(new Uri(endpoint), credential);
+        }
+
+        public async Task<Dictionary<string, string>> AnalyzeInvoice(string filePath)
+        {
+            var extractedData = new Dictionary<string, string>();
+
+            using var stream = new FileStream(filePath, FileMode.Open);
+            AnalyzeDocumentOperation operation = await _client.AnalyzeDocumentAsync(WaitUntil.Completed, "prebuilt-invoice", stream);
+            AnalyzeResult result = operation.Value;
+
+            //Extract key-value pairs from the document
+            foreach (var document in result.Documents)
+            {
+                foreach (var field in document.Fields)
+                {
+                    //add the field key and content to the dictionary
+                    extractedData.Add(field.Key, field.Value.Content);
+                }
+            }
+
+            return extractedData;
+        }
+    }
+}
---
+++ ProjectLedg.Server/Services/IServices/IFormRecognizerService.cs
@@ -0,0 +1,7 @@
+﻿namespace ProjectLedg.Server.Services.IServices
+{
+    public interface IFormRecognizerService
+    {
+        Task AnalyzeForm(string filePath);
+    }
+}
