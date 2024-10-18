8e0651fed257f04541a9203aab39240a5b4d06d4
jonnytouma <jonnytouma@gmail.com>
2024-10-18 13:27:58 +0200

    added bg to whole page

--- projectledg.client/src/pages/LandingPages/FeaturePage.jsx
+++ projectledg.client/src/pages/LandingPages/FeaturePage.jsx
@@ -9,7 +9,8 @@ export default function FeaturePage() {
     return (
         <>
             <Navbar />
-            <div className="w-[100vw] flex flex-col justify-center px-8 md:px-12 lg:px-24">
+            <div className="w-[100vw] flex flex-col justify-center px-8 md:px-12 lg:px-24
+            bg-gradient-to-b from-white to-gray-400">
                 <FeatureContentTitle />
                 <FeatureContentImage />
                 <FeatureContentBody />
