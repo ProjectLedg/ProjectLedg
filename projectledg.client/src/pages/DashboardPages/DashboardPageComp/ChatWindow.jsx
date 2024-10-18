694157807867ce9b62099258d81dc3ea2bd9b139
HjalmarStranninge <hjalmar.stranninge@gmail.com>
2024-10-18 13:12:58 +0200

    Size fix

--- projectledg.client/src/pages/DashboardPages/DashboardPageComp/ChatWindow.jsx
+++ projectledg.client/src/pages/DashboardPages/DashboardPageComp/ChatWindow.jsx
@@ -11,7 +11,7 @@ export default function ChatWindow({ onClose }) {
     }
 
     return (
-        <div className="chatWindow flex flex-col absolute bottom--1 right-0 p-2 w-[40vw] h-[80vh] bg-white/60 bg-opacity-80 shadow-lg rounded-l-2xl max-w-[620px] ml-4">
+        <div className="chatWindow flex flex-col absolute bottom--1 right-0 p-2 w-[30vw] h-[80vh] bg-white/60 bg-opacity-80 shadow-lg rounded-l-2xl max-w-[620px] ml-4">
             <div className="bg-gray-100 rounded-xl overflow-hidden h-[100%]">
                 <div className="flex justify-between items-center p-4">
                     <button onClick={onClose} className="text-gray-500  hover:text-gray-700">
@@ -22,7 +22,7 @@ export default function ChatWindow({ onClose }) {
                     </button>
                 </div>
 
-                <div className="p-6 space-y-4 flex flex-col justify-around h-[80%] mb-8">
+                <div className="p-6 space-y-4 flex flex-col justify-around h-[80%]">
                     <div className="space-y-2 flex flex-col items-center text-center">
                         <h2 className="text-3xl font-normal text-gray-400">
                             Hej <span className="text-green-500">Placeholder</span>
