f0eb755aeccd9324a319b50904858c5ffa5bc041
manhattaa <fadynhatta@gmail.com>
2024-10-20 15:13:28 +0200

    Created DTOs for the sending the information to the Database.

---
+++ ProjectLedg.Server/Data/Models/DTOs/Invoice/InvoiceDTO.cs
@@ -0,0 +1,27 @@
+﻿namespace ProjectLedg.Server.Data.Models.DTOs.Invoice
+{
+    public class InvoiceDTO
+    {
+        public string InvoiceNumber { get; set; }
+        public DateTime InvoiceDate { get; set; }
+        public DateTime DueDate { get; set; }
+        public decimal InvoiceTotal { get; set; }
+        public List<string?> Items { get; set; }
+        public string? PaymentDetails { get; set; }
+        public decimal TotalTax { get; set; }
+        public bool IsPaid { get; set; }
+        public bool IsOutgoing { get; set; }
+        public bool IsBooked { get; set; }
+
+        //Customer Information
+        public string CustomerName { get; set; }
+        public string CustomerAddress { get; set; }
+        public string CustomerAddressRecipient { get; set; }
+
+        //Vendor Information
+        public string VendorName { get; set; }
+        public string VendorAddress { get; set; }
+        public string VendorAddressRecipient { get; set; }
+        public string VendorTaxId { get; set; }
+    }
+}
