48c06c90273e893eab04175da5ebbe833cc96966
Fady <145761517+Manhattaa@users.noreply.github.com>
2024-10-22 14:10:36 +0200

    Created an AzureAI Service layer

---
+++ ProjectLedg.Server/Services/AzureAIService.cs
@@ -0,0 +1,20 @@
+﻿using iTextSharp.text;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.Identity.Client.Extensions.Msal;
+using ProjectLedg.Server.Data.Models;
+using System.Reflection.Metadata;
+using System;
+
+namespace ProjectLedg.Server.Services
+{
+    public class AzureAIService
+    {
+        //Map Invoice to Transactions & BasAccount using AI
+
+        //Give the AI Functions - Look up every BasAccount that has been Booked for Company.
+
+        //Implement integration to Hjalmars Chat Window. - AI Functions - Return every BasAccounts & Transactions for the User; As well as the Company related Queries
+
+        //Implement the.csv file using Azure Blob Storage.
+    }
+}
