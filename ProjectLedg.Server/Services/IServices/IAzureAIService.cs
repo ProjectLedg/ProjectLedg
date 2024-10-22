b4c4f4c18fe54caf139653539c396206d0e965a7
Fady <145761517+Manhattaa@users.noreply.github.com>
2024-10-22 14:10:51 +0200

    The interface of the service layer.

---
+++ ProjectLedg.Server/Services/IServices/IAzureAIService.cs
@@ -0,0 +1,9 @@
+﻿using ProjectLedg.Server.Data.Models;
+
+namespace ProjectLedg.Server.Services.IServices
+{
+    public interface IAzureAIService
+    {
+        Task<string> BookInvoice(Invoice invoice);
+    }
+}
