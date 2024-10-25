646edc68ae42ca22076b63e4bccd0154d83ee299
Fady <145761517+Manhattaa@users.noreply.github.com>
2024-10-25 15:04:20 +0200

    IngoingInvoice Repo layer

--- ProjectLedg.Server/Repositories/IngoingInvoiceRepository.cs
+++ ProjectLedg.Server/Repositories/IngoingInvoiceRepository.cs
@@ -5,7 +5,7 @@ using ProjectLedg.Server.Repositories.IRepositories;
 
 namespace ProjectLedg.Server.Repositories
 {
-    public class IngoingInvoiceRepository : IInvoiceRepository
+    public class IngoingInvoiceRepository : IIngoingInvoiceRepository
     {
         private readonly ProjectLedgContext _context;
 
@@ -14,29 +14,29 @@ namespace ProjectLedg.Server.Repositories
             _context = context;
         }
 
-        public async Task<IngoingInvoice?> GetInvoiceByIdAsync(int invoiceId)
+        public async Task<IngoingInvoice?> GetIngoingInvoiceByIdAsync(int invoiceId)
         {
             return await _context.IngoingInvoices.FindAsync(invoiceId);
         }
 
-        public async Task<IEnumerable<IngoingInvoice>> GetAllInvoicesAsync()
+        public async Task<IEnumerable<IngoingInvoice>> GetAllIngoingInvoicesAsync()
         {
             return await _context.IngoingInvoices.ToListAsync();
         }
 
-        public async Task<bool> CreateInvoiceAsync(IngoingInvoice invoice)
+        public async Task<bool> CreateIngoingInvoiceAsync(IngoingInvoice invoice)
         {
             await _context.IngoingInvoices.AddAsync(invoice);
             return await _context.SaveChangesAsync() > 0;
         }
 
-        public async Task<bool> UpdateInvoiceAsync(IngoingInvoice invoice)
+        public async Task<bool> UpdateIngoingInvoiceAsync(IngoingInvoice invoice)
         {
             _context.IngoingInvoices.Update(invoice);
             return await _context.SaveChangesAsync() > 0;
         }
 
-        public async Task<bool> DeleteInvoiceAsync(int invoiceId)
+        public async Task<bool> DeleteIngoingInvoiceAsync(int invoiceId)
         {
             var invoice = await _context.IngoingInvoices.FindAsync(invoiceId);
             if (invoice == null)
@@ -48,7 +48,7 @@ namespace ProjectLedg.Server.Repositories
             return await _context.SaveChangesAsync() > 0;
         }
 
-        public async Task<bool> SaveInvoiceAsync(IngoingInvoice invoice)
+        public async Task<bool> SaveIngoingInvoiceAsync(IngoingInvoice invoice)
         {
             try
             {
