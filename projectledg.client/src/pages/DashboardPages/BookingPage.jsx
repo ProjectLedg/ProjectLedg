caa0aa857f8e3dcd24d0caa01bd8fc3e57d9a669
Adrozs <adrozsahegyi@gmail.com>
2024-10-27 16:05:07 +0100

    moved invoice state to parent component so that both uploader and preview can access the invoice for upoading and display purposes

--- projectledg.client/src/pages/DashboardPages/BookingPage.jsx
+++ projectledg.client/src/pages/DashboardPages/BookingPage.jsx
@@ -1,19 +1,22 @@
-import React from "react"
+import React, { useState } from "react"
 import InvoiceUploader from "./BookingPageComp/InvoiceUploader"
-import AccountingBotChat from "./BookingPageComp/InvoicePreview"
+import InvoicePreview from "./BookingPageComp/InvoicePreview"
 import InvoiceLogger from "./BookingPageComp/InvoiceLogger"
 
 export default function BookingPage() {
+    // State to hold invoice data for child components
+    const [invoice, setInvoice] = useState(null); 
+
     return (
         <div className="container mx-auto">
         
             <div className="grid grid-cols-1 gap-6">
                 <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
                     <div className="md:col-span-1">
-                        <InvoiceUploader/>
+                        <InvoiceUploader setInvoice={setInvoice}/>
                     </div>
                     <div className="md:col-span-2">
-                        <AccountingBotChat />
+                        <InvoicePreview invoice={invoice}setInvoice={setInvoice}/>
                     </div>
                     
                 </div>
