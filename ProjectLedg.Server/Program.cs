c0b4e78f0a7b466d46ff932b8e9bb7a1c3fe685e
Fady <145761517+Manhattaa@users.noreply.github.com>
2024-10-28 15:59:49 +0100

    Added the new Azure Database connection string endpoint.

--- ProjectLedg.Server/Program.cs
+++ ProjectLedg.Server/Program.cs
@@ -70,7 +70,7 @@ namespace ProjectLedg.Server
             services.AddDbContext<ProjectLedgContext>(options =>
             {
                 options.UseSqlServer(Environment.GetEnvironmentVariable("CONNECTION_STRING"));
-                //options.UseSqlServer(Environment.GetEnvironmentVariable("AZURE_DATABASE_CONNECTION_STRING"));
+                //options.UseSqlServer(Environment.GetEnvironmentVariable("LEDGEDB_CONNECTION_STRING"));
             });
 
             // Add services to the container.
