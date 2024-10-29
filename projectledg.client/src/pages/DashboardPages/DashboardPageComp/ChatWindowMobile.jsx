b36d89d8a53eab3b6c4a86dff0dca87fcb9f43ee
HjalmarStranninge <hjalmar.stranninge@gmail.com>
2024-10-29 13:22:55 +0100

    Added logic to send and recieve message from api

--- projectledg.client/src/pages/DashboardPages/DashboardPageComp/ChatWindowMobile.jsx
+++ projectledg.client/src/pages/DashboardPages/DashboardPageComp/ChatWindowMobile.jsx
@@ -1,22 +1,42 @@
-import React, { useState } from 'react'
-import { X, Send, FilePlus, Undo2, FileChartColumn, SquarePen, Paperclip } from 'lucide-react'
+import React, { useState } from 'react';
+import { X, Send, FilePlus, Undo2, FileChartColumn, SquarePen, Paperclip } from 'lucide-react';
 
 export default function ChatWindow({ onClose, onSendMessage }) {
-    const [input, setInput] = useState('')
+    const [input, setInput] = useState('');
+    const [messages, setMessages] = useState([]); // State to hold the array of messages
+    const [hasMessages, setHasMessages] = useState(false);
 
-    const handleSubmit = (e) => {
+    const handleSubmit = async (e) => {
         e.preventDefault();
-        if (input.trim()) {  // Ensure message is not empty
-            onSendMessage(input);  // Send input to ChatService
-            setInput('');  // Clear input after sending
+        if (input.trim()) {
+            const newMessage = { text: input, type: 'sent' };
+            setMessages((prevMessages) => [...prevMessages, newMessage]);
+            setHasMessages(true); // Set hasMessages to true when a message is sent
+            const response = await handleSend(input);
+
+            const responseMessage = { text: response, type: 'received' };
+            setMessages((prevMessages) => [...prevMessages, responseMessage]);
+
+            setInput('');
+        }
+    };
+
+    const handleSend = async (input) => {
+        try {
+            const response = await onSendMessage(input);
+            console.log('Response from server:', response);
+            return response || 'No response received';
+        } catch (error) {
+            console.error("Error while sending message:", error);
+            return 'Error occurred while sending message';
         }
     };
 
     return (
-        <div className="chatWindow flex flex-col  right-0 p-2 w-full h-[100vh] bg-white/60 bg-opacity-80 shadow-lg rounded-l-2xl ">
-            <div className="bg-gray-100 rounded-xl overflow-hidden h-[100%]">
-                <div className="flex justify-between items-center p-4 ">
-                    <button onClick={onClose} className="text-gray-500  hover:text-gray-700">
+        <div className="chatWindow flex flex-col right-0 p-2 w-full h-[100vh] bg-white/60 bg-opacity-80 shadow-lg rounded-l-2xl">
+            <div className="bg-gray-100 rounded-xl overflow-hidden h-[100%] relative flex flex-col">
+                <div className="flex justify-between items-center p-4">
+                    <button onClick={onClose} className="text-gray-500 hover:text-gray-700">
                         <SquarePen size={25} />
                     </button>
                     <button onClick={onClose} className="text-gray-500 bg-gray-200 p-1 rounded-full hover:text-gray-700">
@@ -24,43 +44,55 @@ export default function ChatWindow({ onClose, onSendMessage }) {
                     </button>
                 </div>
 
-                <div className="p-6 space-y-4 flex flex-col justify-around h-[85%]">
-                    <div className="space-y-2 flex flex-col items-center text-center">
-                        <h2 className="text-3xl font-normal text-gray-400">
-                            Hej <span className="text-green-500">Placeholder</span>
-                        </h2>
+                {!hasMessages ? (
+                    <div className="p-6 space-y-4 flex flex-col justify-around h-[80%]">
+                        <div className="space-y-2 flex flex-col items-center text-center">
+                            <h2 className="text-3xl font-normal text-gray-400">
+                                Hej <span className="text-green-500">Placeholder</span>
+                            </h2>
+                            <h3 className="text-xl font-semibold text-gray-800">Kan jag hjälpa dig med något?</h3>
+                            <p className="text-md text-gray-600">
+                                Oavsett om det gäller att hålla koll på utgifter, skapa fakturor eller något annat inom bokföring, finns jag här för dig!
+                            </p>
+                        </div>
 
-                        <h3 className="text-xl font-semibold text-gray-800">Kan jag hjälpa dig med något?</h3>
-                        <p className="text-md text-gray-600">
-                            Oavsett om det gäller att hålla koll på utgifter, skapa fakturor eller något annat inom bokföring, finns jag här för dig!
-                        </p>
+                        <div className="space-y-2 flex flex-col justify-between h-[45%]">
+                            {['Hur skapar jag en faktura?', 'Vilka utgifter är avdragsgilla?', 'Visa min senaste månatliga rapport'].map((topic, index) => (
+                                <button
+                                    key={index}
+                                    className="w-full text-left p-3 h-20 bg-white rounded-lg shadow-sm hover:shadow-md transition-shadow duration-200 flex items-center space-x-2"
+                                >
+                                    <div className="w-8 h-8 text-gray-100 bg-gray-800 rounded-xl flex items-center justify-center">
+                                        {index === 0 ? (
+                                            <FilePlus className="w-4 h-4" />
+                                        ) : index === 1 ? (
+                                            <Undo2 className="w-4 h-4" />
+                                        ) : (
+                                            <FileChartColumn className="w-4 h-4" />
+                                        )}
+                                    </div>
+                                    <span className="text-sm font-medium text-gray-800">{topic}</span>
+                                </button>
+                            ))}
+                        </div>
                     </div>
-
-                    <div className="space-y-2 flex flex-col h-[45%]">
-                        {['Hur skapar jag en faktura?', 'Vilka utgifter är avdragsgilla?', 'Visa min senaste månatliga rapport'].map((topic, index) => (
-                            <button
+                ) : (
+                    <div className="flex flex-col p-4 overflow-y-auto flex-grow mb-">
+                        {messages.map((message, index) => (
+                            <div
                                 key={index}
-                                className="w-full text-left p-3 h-20 bg-white rounded-lg shadow-sm hover:shadow-md transition-shadow duration-200 flex items-center space-x-2"
+                                className={`max-w-[80%] p-4 rounded-3xl shadow-md mb-2 ${message.type === 'sent' ? 'bg-green-500 text-white self-end' : 'bg-transparent text-black self-start'}`}
                             >
-                                <div className="w-8 h-8 text-gray-100 bg-gray-800 rounded-xl flex items-center justify-center">
-                                    {index === 0 ? (
-                                        <FilePlus className="w-4 h-4" />
-                                    ) : index === 1 ? (
-                                        <Undo2 className="w-4 h-4" />
-                                    ) : (
-                                        <FileChartColumn className="w-4 h-4" />
-                                    )}
-                                </div>
-                                <span className="text-sm font-medium text-gray-800">{topic}</span>
-                            </button>
+                                <p className="text-md font-normal">{message.text}</p>
+                            </div>
                         ))}
                     </div>
-                </div>
+                )}
 
-                <form onSubmit={handleSubmit} className=" bg-transparent">
+                <form onSubmit={handleSubmit} className="absolute bottom-0 left-0 right-0 bg-transparent p-1">
                     <div className="flex flex-row justify-between items-center space-x-2">
-                        <div className="flex-grow p-2 h-14 bg-white rounded-full  flex items-center">
-                            <button type="submit" className="p-2 bg-gray-800 text-white rounded-full hover:bg-gray-500 transition-colors duration-200">
+                        <div className="flex-grow p-2 h-14 bg-white rounded-full flex items-center">
+                            <button type="button" className="p-2 bg-gray-800 text-white rounded-full hover:bg-gray-500 transition-colors duration-200">
                                 <Paperclip size={20} />
                             </button>
                             <input
@@ -68,7 +100,7 @@ export default function ChatWindow({ onClose, onSendMessage }) {
                                 value={input}
                                 onChange={(e) => setInput(e.target.value)}
                                 placeholder="Fråga Ledge..."
-                                className="flex-grow p-2 bg-transparent rounded-full focus:outline-none "
+                                className="flex-grow p-2 bg-transparent rounded-full focus:outline-none"
                             />
                             <button type="submit" className="p-2 bg-green-500 text-white rounded-full hover:bg-green-600 transition-colors duration-200">
                                 <Send size={20} />
@@ -76,11 +108,7 @@ export default function ChatWindow({ onClose, onSendMessage }) {
                         </div>
                     </div>
                 </form>
-
-
             </div>
-
-
         </div>
-    )
+    );
 }
