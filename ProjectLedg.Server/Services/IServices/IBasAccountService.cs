404ac3273edad9c98447d74689c833c71839ab2b
manhattaa <fadynhatta@gmail.com>
2024-10-29 17:38:32 +0100

    BasAccountService layer for Scanning BasAccountPlan

---
+++ ProjectLedg.Server/Services/IServices/IBasAccountService.cs
@@ -0,0 +1,9 @@
+﻿using ProjectLedg.Server.Data.Models;
+
+namespace ProjectLedg.Server.Services.IServices
+{
+    public interface IBasAccountService
+    {
+        List<BasAccount> GetBasAccounts();
+    }
+}
