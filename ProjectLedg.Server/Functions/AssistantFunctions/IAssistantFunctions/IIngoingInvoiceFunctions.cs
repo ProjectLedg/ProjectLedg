ed4b0fda24c876764afdcac5c62961bebe6e0acf
manhattaa <fadynhatta@gmail.com>
2024-11-03 21:47:38 +0100

    IngoingInvoiceFunctions complete with an Interface layer.

---
+++ ProjectLedg.Server/Functions/AssistantFunctions/IAssistantFunctions/IIngoingInvoiceFunctions.cs
@@ -0,0 +1,8 @@
+﻿namespace ProjectLedg.Server.Functions.AssistantFunctions.IAssistantFunctions
+{
+    public interface IIngoingInvoiceFunctions
+    {
+        Task<string> GetUnpaidIngoingInvoicesForCompany(int companyId);
+        Task<string> GetInvoices(string companyName, int? year = null, int? month = null);
+    }
+}
