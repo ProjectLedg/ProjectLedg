dae56d032438567b505939f0eac489b675aba5ce
manhattaa <fadynhatta@gmail.com>
2024-11-03 21:48:18 +0100

    TransactionFunctions complete with an Interface layer,

---
+++ ProjectLedg.Server/Functions/AssistantFunctions/IAssistantFunctions/ITransactionFunctions.cs
@@ -0,0 +1,8 @@
+﻿namespace ProjectLedg.Server.Functions.AssistantFunctions.IAssistantFunctions
+{
+    public interface ITransactionFunctions
+    {
+        Task<string> GetHighProfileTransactionsForCompany(int companyId, decimal threshold = 10000);
+        Task<string> GetLatestTransactionsForCompany(int companyId, int count = 5);
+    }
+}
