ec45366c251f519cd5d97d4f43bb393f35f64341
manhattaa <fadynhatta@gmail.com>
2024-11-03 21:46:53 +0100

    BasAccount Functions complete with an Interface layer

---
+++ ProjectLedg.Server/Functions/AssistantFunctions/IAssistantFunctions/IBasAccountFunctions.cs
@@ -0,0 +1,9 @@
+﻿namespace ProjectLedg.Server.Functions.AssistantFunctions.IAssistantFunctions;
+
+public interface IBasAccountFunctions
+{
+    Task<string> HandleGetCompanyBasAccounts(string[] args);
+    Task<string> FetchCompanyBasAccountsAsync(int companyId);
+    Task<string> GetPopularBasAccountsForCompany(int companyId);
+    Task<string> GetCompanyBasAccounts(int companyId);
+}
