d7f93866cd71b9bca004ad9f0151e3f4d4e95460
Adrian Rozsahegyi <122897788+Adrozs@users.noreply.github.com>
2024-11-04 11:25:23 +0100

    Updated the switch component with framer motion to add some springy animation to it when switched

--- projectledg.client/src/components/ui/switch.jsx
+++ projectledg.client/src/components/ui/switch.jsx
@@ -1,22 +1,24 @@
 import * as React from "react"
-import * as SwitchPrimitives from "@radix-ui/react-switch"
+import { motion } from "framer-motion"
 
-import { cn } from "@/lib/utils"
 
-const Switch = React.forwardRef(({ className, ...props }, ref) => (
-  <SwitchPrimitives.Root
-    className={cn(
-    "peer inline-flex h-6 w-11 shrink-0 cursor-pointer items-center rounded-full border-2 border-transparent transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 focus-visible:ring-offset-background disabled:cursor-not-allowed disabled:opacity-50 data-[state=checked]:bg-green-500 data-[state=unchecked]:bg-red-500",
-      className
-    )}
-    {...props}
-    ref={ref}>
-    <SwitchPrimitives.Thumb
-      className={cn(
-        "pointer-events-none block h-5 w-5 rounded-full bg-background shadow-lg ring-0 transition-transform data-[state=checked]:translate-x-5 data-[state=unchecked]:translate-x-0"
-      )} />
-  </SwitchPrimitives.Root>
-))
-Switch.displayName = SwitchPrimitives.Root.displayName
 
-export { Switch }
+
+export default function Switch ({isOn, setIsOn}) {
+
+  const toggleSwitch = () => setIsOn(!isOn)
+
+  return (
+    <div
+      data-isOn={isOn}
+      onClick={toggleSwitch}
+      className={`flex ${isOn ? "justify-end bg-green-600" : "justify-start  bg-red-600"}  p-1 w-10 h-6 cursor-pointer  rounded-3xl `}
+      style={{
+        transition: "background-color 0.3s ease"
+      }}>
+
+      <motion.div className="bg-white w-[50%] h-[100%] rounded-full" layout transition={{ type: "spring", stiffness: 700, damping: 30 }} />
+    </div>
+  )
+}
+  
