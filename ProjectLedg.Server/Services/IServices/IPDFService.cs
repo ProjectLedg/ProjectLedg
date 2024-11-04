07324677c66cdf8c42de5b13a9d8ba5045eae9fc
ChasAlex <alexander.lundquist@chasacademy.se>
2024-11-04 10:53:10 +0100

    Made the service function for it

--- ProjectLedg.Server/Services/IServices/IPDFService.cs
+++ ProjectLedg.Server/Services/IServices/IPDFService.cs
@@ -1,11 +1,12 @@
-﻿using ProjectLedg.Server.Data.Models.DTOs.Invoice;
+﻿using ProjectLedg.Server.Data.Models.DTOs.Finance;
+using ProjectLedg.Server.Data.Models.DTOs.Invoice;
 using ProjectLedg.Server.Data.Models.DTOs.PDF;
 
 namespace ProjectLedg.Server.Services.IServices
 {
     public interface IPDFService
     {
-        byte[] GenerateAnnualReportPdf();
+        Task<byte[]> GenerateAnnualReportPdf(AnualReportRequestDTO dto);
         Task<byte[]> GenerateInvoicePdf(OutgoingInvoiceGenerationDTO dto);
     }
 }
