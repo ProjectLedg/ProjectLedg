cd85248ca7fd07cbeaf9fce0c86191e4c5d95bb2
jonnytouma <jonnytouma@gmail.com>
2024-11-05 16:05:05 +0100

    annualreport layout

--- projectledg.client/src/App.jsx
+++ projectledg.client/src/App.jsx
@@ -3,16 +3,16 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import LandingPage from './pages/LandingPages/LandingPage';
 import LoginPage from './pages/AccountHandlingPages/LoginPage';
 import SignupPage from './pages/AccountHandlingPages/SignupPage';
-import CompanySelectPage  from './pages/AccountHandlingPages/CompanySelectPage'
-import CompanyCreatePage  from './pages/AccountHandlingPages/CompanyCreatePage'
+import CompanySelectPage from './pages/AccountHandlingPages/CompanySelectPage'
+import CompanyCreatePage from './pages/AccountHandlingPages/CompanyCreatePage'
 import ContactPage from './pages/LandingPages/ContactPage'
 import PricingPage from './pages/LandingPages/PricingPage'
 import FeaturePage from './pages/LandingPages/FeaturePage'
-import WhyPage from  './pages/LandingPages/WhyPage'
+import WhyPage from './pages/LandingPages/WhyPage'
 import DashboardLayout from './pages/DashboardPages/DashboardPageComp/DashboardLayout'
 import DashboardHomePage from './pages/DashboardPages/DashboardHomePage'
 import FinancialReportsPage from './pages/DashboardPages/FinancialReportsPage'
-import FinancialStatementPage from './pages/DashboardPages/FinancialStatementPage'
+import AnnualReportPage from './pages/DashboardPages/AnnualReportPage';
 import SettingsPage from './pages/DashboardPages/SettingsPage'
 import BookingPage from './pages/DashboardPages/BookingPage'
 import InvoicingPage from './pages/DashboardPages/InvoicingPage';
@@ -22,25 +22,25 @@ function App() {
         <Router>
             <Routes>
                 <Route path="/" element={<LandingPage />} />
-                <Route path="/login" element={<LoginPage/>}/>
-                <Route path="/signup" element={<SignupPage/>}/>
-                <Route path="/company-select" element={<CompanySelectPage/>}/>
+                <Route path="/login" element={<LoginPage />} />
+                <Route path="/signup" element={<SignupPage />} />
+                <Route path="/company-select" element={<CompanySelectPage />} />
                 <Route path="/dashboard/:companyId" element={<DashboardLayout />}>
                     {/* Nested Routes */}
                     <Route index element={<DashboardHomePage />} />
                     <Route path="financial-reports" element={<FinancialReportsPage />} />
                     <Route path="book" element={<BookingPage />} />
-                    <Route path="financial-statement" element={<FinancialStatementPage />} />
+                    <Route path="annual-report" element={<AnnualReportPage />} />
                     <Route path="invoicing" element={<InvoicingPage />} />
                     <Route path="settings" element={<SettingsPage />} />
                 </Route>
-                <Route path="/why" element={<WhyPage/>}/>
-                <Route path="/features" element={<FeaturePage/>}/>
-                <Route path="/pricing" element={<PricingPage/>}/>
-                <Route path="/contact" element={<ContactPage/>}/>
-                <Route path="/company-create" element={<CompanyCreatePage/>}/>      
+                <Route path="/why" element={<WhyPage />} />
+                <Route path="/features" element={<FeaturePage />} />
+                <Route path="/pricing" element={<PricingPage />} />
+                <Route path="/contact" element={<ContactPage />} />
+                <Route path="/company-create" element={<CompanyCreatePage />} />
             </Routes>
         </Router>
-    );    
+    );
 }
 export default App;
