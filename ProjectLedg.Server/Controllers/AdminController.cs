f19b33c605fdc464d99ef282edfed5b2685906f8
Fady <145761517+Manhattaa@users.noreply.github.com>
2024-11-07 16:13:15 +0100

    Admincontroller setup

---
+++ ProjectLedg.Server/Controllers/AdminController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ProjectLedg.Server.Services.IServices;
+
+namespace ProjectLedg.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AdminController : ControllerBase
+    {
+        private readonly ILogger<AdminController> _logger;
+        private readonly IAdminService _adminService;
+
+        public AdminController(ILogger<AdminController> logger, IAdminService adminService)
+        {
+            _logger = logger;
+            _adminService = adminService;
+        }
+    }
+}
