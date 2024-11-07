0d69b8c2234d1d6ce4db219e6ae3faeba31825ce
Fady <145761517+Manhattaa@users.noreply.github.com>
2024-11-07 16:13:38 +0100

    Admin Service layer

---
+++ ProjectLedg.Server/Services/AdminService.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Identity;
+using ProjectLedg.Server.Data.Models;
+using ProjectLedg.Server.Model.DTOs.User;
+using ProjectLedg.Server.Options;
+using ProjectLedg.Server.Services.IServices;
+using System.Security.Claims;
+
+namespace ProjectLedg.Server.Services
+{
+    public class AdminService : IAdminService
+    {
+        public Task<LoginResult> AdminLoginAsync(string email, string password)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<AccountCreationResult> CreateAdminsAsync(CreateAccountRequestDTO request)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IdentityResult> DeleteAdminsAsync(string password, ClaimsPrincipal currentUser)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<User>> GetAllAdminsAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<User> GetUserById(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IdentityResult> SendEmailVerificationAsync(string userId, string code)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IdentityResult> UpdateAdminsAsync(User user)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ ProjectLedg.Server/Services/IServices/IAdminService.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Identity;
+using ProjectLedg.Server.Data.Models;
+using ProjectLedg.Server.Model.DTOs.User;
+using ProjectLedg.Server.Options;
+using System.Security.Claims;
+
+namespace ProjectLedg.Server.Services.IServices
+{
+    public interface IAdminService
+    {
+        Task<IEnumerable<User>> GetAllAdminsAsync();
+        Task<User> GetUserById(string id);
+        Task<AccountCreationResult> CreateAdminsAsync(CreateAccountRequestDTO request);
+        Task<IdentityResult> UpdateAdminsAsync(User user);
+        Task<IdentityResult> DeleteAdminsAsync(string password, ClaimsPrincipal currentUser);
+        Task<LoginResult> AdminLoginAsync(string email, string password);
+        Task<IdentityResult> SendEmailVerificationAsync(string userId, string code);
+    }
+}
