b71994790db8540e25d9811b5ed36db807a2a4dd
Fady <145761517+Manhattaa@users.noreply.github.com>
2024-11-07 16:13:27 +0100

    Admin Repository layer

---
+++ ProjectLedg.Server/Repositories/AdminRepository.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Identity;
+using ProjectLedg.Server.Data.Models;
+using ProjectLedg.Server.Options;
+using ProjectLedg.Server.Repositories.IRepositories;
+using System.Security.Claims;
+
+namespace ProjectLedg.Server.Repositories
+{
+    public class AdminRepository : IAdminRepository
+    {
+        public Task<IdentityResult> CreateAdminAsync(User user, string password)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IdentityResult> DeleteAdminAsync(string password, ClaimsPrincipal currentUser)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<User> GetAdminByEmailAsync(string email)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<User> GetAdminsById(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<User>> GetAllAdminsAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<LoginResult> LoginAsync(string email, string password)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IdentityResult> SendEmailVerificationAsync(string userId, string code)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IdentityResult> UpdateAdminAsync(User user)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ ProjectLedg.Server/Repositories/IRepositories/IAdminRepository.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Identity;
+using ProjectLedg.Server.Data.Models;
+using ProjectLedg.Server.Options;
+using System.Security.Claims;
+
+namespace ProjectLedg.Server.Repositories.IRepositories
+{
+    public interface IAdminRepository
+    {
+        Task<IEnumerable<User>> GetAllAdminsAsync();
+        Task<User> GetAdminsById(string id);
+        Task<IdentityResult> CreateAdminAsync(User user, string password);
+        Task<IdentityResult> UpdateAdminAsync(User user);
+        Task<IdentityResult> DeleteAdminAsync(string password, ClaimsPrincipal currentUser);
+        Task<LoginResult> LoginAsync(string email, string password);
+        Task<IdentityResult> SendEmailVerificationAsync(string userId, string code);
+        Task<User> GetAdminByEmailAsync(string email);
+    }
+}
