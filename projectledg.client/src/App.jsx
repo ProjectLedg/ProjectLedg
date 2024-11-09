c5c2cd3d9b3ad18e7a9ec8fc8d3eacb5156aaa27
manhattaa <fadynhatta@gmail.com>
2024-11-09 14:45:10 +0100

    Routing for the new Admin related pages.

--- projectledg.client/src/App.jsx
+++ projectledg.client/src/App.jsx
@@ -3,26 +3,29 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import LandingPage from './pages/LandingPages/LandingPage';
 import LoginPage from './pages/AccountHandlingPages/LoginPage';
 import SignupPage from './pages/AccountHandlingPages/SignupPage';
-import CompanySelectPage  from './pages/AccountHandlingPages/CompanySelectPage'
-import CompanyCreatePage  from './pages/AccountHandlingPages/CompanyCreatePage'
-import ContactPage from './pages/LandingPages/ContactPage'
-import PricingPage from './pages/LandingPages/PricingPage'
-import FeaturePage from './pages/LandingPages/FeaturePage'
-import WhyPage from  './pages/LandingPages/WhyPage'
-import DashboardLayout from './pages/DashboardPages/DashboardPageComp/DashboardLayout'
-import DashboardHomePage from './pages/DashboardPages/DashboardHomePage'
-import FinancialReportsPage from './pages/DashboardPages/FinancialReportsPage'
-import FinancialStatementPage from './pages/DashboardPages/FinancialStatementPage'
-import SettingsPage from './pages/DashboardPages/SettingsPage'
-import BookingPage from './pages/DashboardPages/BookingPage'
+import CompanySelectPage from './pages/AccountHandlingPages/CompanySelectPage';
+import CompanyCreatePage from './pages/AccountHandlingPages/CompanyCreatePage';
+import ContactPage from './pages/LandingPages/ContactPage';
+import PricingPage from './pages/LandingPages/PricingPage';
+import FeaturePage from './pages/LandingPages/FeaturePage';
+import WhyPage from './pages/LandingPages/WhyPage';
+import DashboardLayout from './pages/DashboardPages/DashboardPageComp/DashboardLayout';
+import DashboardHomePage from './pages/DashboardPages/DashboardHomePage';
+import FinancialReportsPage from './pages/DashboardPages/FinancialReportsPage';
+import FinancialStatementPage from './pages/DashboardPages/FinancialStatementPage';
+import SettingsPage from './pages/DashboardPages/SettingsPage';
+import BookingPage from './pages/DashboardPages/BookingPage';
 import InvoicingPage from './pages/DashboardPages/InvoicingPage';
 import HelpPage from './pages/DashboardPages/HelpPage';
+//Admin
 import AdminLoginPage from './pages/Admin/AdminLoginPage';
-import AdminDashboard from './pages/Admin/AdminDashboard';
+import AdminDashboardHomePage from './pages/Admin/AdminDashboards/AdminDashboardHomePage';
+import AdminDashboardLayout from './pages/Admin/AdminDashboards/AdminDashboardLayout'
 import ProtectedRoute from './pages/Admin/ProtectedRouteComp/ProtectedRoute';
-import UserManagement from './pages/Admin/UserManagement';
+import UserTickets from './pages/Admin/AdminDashboards/UserTickets'
+import AdminSettingsPage from './pages/Admin/AdminDashboards/AdminSettingsPage'
+import UserManagementPage from './pages/Admin/AdminDashboards/UserManagementPage'
 import { ThemeProvider } from './components/ThemeProvider';
-
 
 function App() {
     return (
@@ -30,11 +33,10 @@ function App() {
             <Router>
                 <Routes>
                     <Route path="/" element={<LandingPage />} />
-                    <Route path="/login" element={<LoginPage/>}/>
-                    <Route path="/signup" element={<SignupPage/>}/>
-                    <Route path="/company-select" element={<CompanySelectPage/>}/>                    
+                    <Route path="/login" element={<LoginPage />} />
+                    <Route path="/signup" element={<SignupPage />} />
+                    <Route path="/company-select" element={<CompanySelectPage />} />
                     <Route path="/dashboard/:companyId" element={<DashboardLayout />}>
-                        {/* Nested Routes */}
                         <Route index element={<DashboardHomePage />} />
                         <Route path="financial-reports" element={<FinancialReportsPage />} />
                         <Route path="book" element={<BookingPage />} />
@@ -43,17 +45,29 @@ function App() {
                         <Route path="settings" element={<SettingsPage />} />
                         <Route path="help" element={<HelpPage />} />
                     </Route>
-                    <Route path="/why" element={<WhyPage/>}/>
-                    <Route path="/features" element={<FeaturePage/>}/>
-                    <Route path="/pricing" element={<PricingPage/>}/>
-                    <Route path="/contact" element={<ContactPage/>}/>
-                    <Route path="/company-create" element={<CompanyCreatePage/>}/>
-                    <Route path="/admin-login" element={<AdminLoginPage />}/>
-                <Route path="/admin" element={<ProtectedRoute role="Admin,Manager"><AdminDashboard /></ProtectedRoute>} />
-                <Route path="/admin/users" element={<ProtectedRoute role="Admin,Manager"><UserManagement /></ProtectedRoute>} />
+                    <Route path="/why" element={<WhyPage />} />
+                    <Route path="/features" element={<FeaturePage />} />
+                    <Route path="/pricing" element={<PricingPage />} />
+                    <Route path="/contact" element={<ContactPage />} />
+                    <Route path="/company-create" element={<CompanyCreatePage />} />
+                    <Route path="/admin-login" element={<AdminLoginPage />} />
+
+                    {/* Protected Routes for Admin and Manager roles */}
+                    <Route path="/admin/dashboard"
+                        element={
+                            <ProtectedRoute requiredRoles={["Admin", "Manager"]}>
+                            <AdminDashboardLayout />
+                            </ProtectedRoute>
+                        } >
+                        <Route index element={<AdminDashboardHomePage />} />
+                        <Route path="settings" element={<AdminSettingsPage />} />
+                        <Route path="tickets" element={<UserTickets />} />
+                        <Route path="usermanagement" element={<UserManagementPage />} />
+                        </Route>
                 </Routes>
             </Router>
         </ThemeProvider>
-    );    
+    );
 }
+
 export default App;
