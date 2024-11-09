e9ed09d0179a5e7bd970f5598fcf0897f877ff28
manhattaa <fadynhatta@gmail.com>
2024-11-09 19:55:28 +0100

    SupportTicket Repository layer.

---
+++ ProjectLedg.Server/Repositories/IRepositories/ISupportTicketRepository.cs
@@ -0,0 +1,12 @@
+﻿using ProjectLedg.Server.Data.Models;
+
+namespace ProjectLedg.Server.Repositories.IRepositories
+{
+    public interface ISupportTicketRepository
+    {
+        Task<SupportTicket> CreateTicketAsync(SupportTicket ticket);
+        Task<IEnumerable<SupportTicket>> GetAllTicketsAsync();
+        Task<SupportTicket> GetTicketByIdAsync(int ticketId);
+        Task UpdateTicketStatusAsync(int ticketId, string status);
+    }
+}
