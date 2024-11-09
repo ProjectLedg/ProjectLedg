42b33d7b68f0201970fd75df9c7d412485b38c81
manhattaa <fadynhatta@gmail.com>
2024-11-09 19:55:38 +0100

    Support Ticket Service layer!

---
+++ ProjectLedg.Server/Services/IServices/ISupportTicketService.cs
@@ -0,0 +1,14 @@
+﻿using ProjectLedg.Server.Data.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace ProjectLedg.Server.Services.IServices
+{
+    public interface ISupportTicketService
+    {
+        Task<SupportTicket> CreateTicketAsync(SupportTicket ticket, int companyId);
+        Task<IEnumerable<SupportTicket>> GetAllTicketsAsync();
+        Task<SupportTicket> GetTicketByIdAsync(int ticketId);
+        Task UpdateTicketStatusAsync(int ticketId, string status);
+    }
+}
