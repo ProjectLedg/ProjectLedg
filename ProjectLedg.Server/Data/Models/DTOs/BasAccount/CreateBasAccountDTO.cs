86ed7417f34ceb1880c7ce870123dec7efe1aff2
Adrian Rozsahegyi <122897788+Adrozs@users.noreply.github.com>
2024-11-09 22:43:21 +0100

    Added DTOs for the invoice, bas acc, transaction creation

---
+++ ProjectLedg.Server/Data/Models/DTOs/BasAccount/CreateBasAccountDTO.cs
@@ -0,0 +1,15 @@
+﻿//namespace ProjectLedg.Server.Data.Models.DTOs.BasAccount
+//{
+//    public class CreateBasAccountDTO
+//    {
+//        public List<BasAccountDTO> accounts;
+//    }
+
+//    public class BasAccountDTO
+//    {
+//        public string BasAccount { get; set; }
+//        public string Description { get; set; }
+//        public decimal Debit { get; set; }
+//        public decimal Credit { get; set; }
+//    }
+//}
---
+++ ProjectLedg.Server/Data/Models/DTOs/BasAccount/InvoiceAndBasAccountDto.cs
@@ -0,0 +1,22 @@
+﻿using ProjectLedg.Server.Data.Models.DTOs.Invoice;
+
+namespace ProjectLedg.Server.Data.Models.DTOs.BasAccount
+{
+    public class InvoiceAndBasAccountDto
+    {
+        public int CompanyId { get; set; }
+
+        public InvoiceDTO Invoice { get; set; }
+
+        public List<BasAccountDTO> Accounts { get; set; }
+
+    }
+
+    public class BasAccountDTO
+    {
+        public string BasAccount { get; set; }
+        public string Description { get; set; }
+        public decimal Debit { get; set; }
+        public decimal Credit { get; set; }
+    }
+}
