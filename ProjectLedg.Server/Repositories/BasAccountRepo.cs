d1a3e6189e456cfe0842541b71eb2fc97ee0d983
Adrian Rozsahegyi <122897788+Adrozs@users.noreply.github.com>
2024-11-09 22:46:44 +0100

    Created method to create bas account

---
+++ ProjectLedg.Server/Repositories/BasAccountRepo.cs
@@ -0,0 +1,23 @@
+﻿using ProjectLedg.Server.Data;
+using ProjectLedg.Server.Data.Models;
+using ProjectLedg.Server.Repositories.IRepositories;
+
+namespace ProjectLedg.Server.Repositories
+{
+    public class BasAccountRepo : IBasAccountRepo
+    {
+        private readonly ProjectLedgContext _context;
+
+        public BasAccountRepo(ProjectLedgContext context)
+        {
+            _context = context;
+        }
+
+
+        public async Task CreateBasAccountAsync(BasAccount basAccount)
+        {
+            await _context.BasAccounts.AddAsync(basAccount);
+            await _context.SaveChangesAsync(); 
+        }
+    }
+}
