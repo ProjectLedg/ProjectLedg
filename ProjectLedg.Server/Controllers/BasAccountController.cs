bc0d77899aea8e1c9d81aa2f945164c2ba1350a8
Adrian Rozsahegyi <122897788+Adrozs@users.noreply.github.com>
2024-11-09 22:39:56 +0100

    Created controller for bas account which creates invoice then creates and connects bas account, transaction with said invoice

---
+++ ProjectLedg.Server/Controllers/BasAccountController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProjectLedg.Server.Data.Models.DTOs.BasAccount;
+using ProjectLedg.Server.Services.IServices;
+using System.Security.Claims;
+
+namespace ProjectLedg.Server.Controllers
+{
+    [Route("/api/[controller]")]
+    [ApiController]
+    public class BasAccountController : ControllerBase
+    {
+        private readonly IBasAccountService _basAccountService;
+        private readonly IIngoingInvoiceService _ingoingInvoiceService;
+
+        public BasAccountController(IBasAccountService basAccountService, IIngoingInvoiceService ingoingInvoiceService)
+        {
+            _basAccountService = basAccountService;
+            _ingoingInvoiceService = ingoingInvoiceService;
+        }
+
+
+        // TODO: ADD AUTHORIZATION WHEN WERE DONE TESTING
+        [HttpPost("")]
+        public async Task<IActionResult> ProcessInvoiceWithBasAccounts([FromBody] InvoiceAndBasAccountDto invBaAccDto)
+        {
+
+            var invoice = await _ingoingInvoiceService.CreateIngoingInvoiceAsync(invBaAccDto.Invoice);
+            if (invoice == null)
+                return BadRequest("Failed to create invoice");
+
+            var basAccountResult = _basAccountService.AddBasAccountsToCompanyAsync(invBaAccDto.Accounts, invoice, invBaAccDto.CompanyId);
+            if (basAccountResult == null)
+                return BadRequest("Failed to create BAS Account");
+
+            return Ok("Invoice created and mapped to BAS Accounts");
+        }
+
+    }
+}
