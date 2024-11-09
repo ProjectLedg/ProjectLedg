639e968e85bea9f75e8ffd4c79b190f2a8e50ce4
Adrian Rozsahegyi <122897788+Adrozs@users.noreply.github.com>
2024-11-09 22:45:12 +0100

    created interfaces for bas acc and transaction service and repo for DI

---
+++ ProjectLedg.Server/Repositories/IRepositories/IBasAccountRepo.cs
@@ -0,0 +1,9 @@
+﻿using ProjectLedg.Server.Data.Models;
+
+namespace ProjectLedg.Server.Repositories.IRepositories
+{
+    public interface IBasAccountRepo
+    {
+        Task CreateBasAccountAsync(BasAccount basAccount);
+    }
+}
--- ProjectLedg.Server/Services/IServices/IBasAccountService.cs
+++ ProjectLedg.Server/Services/IServices/IBasAccountService.cs
@@ -1,9 +1,16 @@
-﻿using ProjectLedg.Server.Data.Models;
+﻿using Microsoft.AspNetCore.Mvc;
+using ProjectLedg.Server.Data.Models;
+using ProjectLedg.Server.Data.Models.DTOs.BasAccount;
+using ProjectLedg.Server.Data.Models.DTOs.Invoice;
+using ProjectLedg.Server.Options;
+using System.Security.Claims;
 
 namespace ProjectLedg.Server.Services.IServices
 {
     public interface IBasAccountService
     {
         List<BasAccount> GetBasAccounts();
+        Task<ResultObject> AddBasAccountsToCompanyAsync(List<BasAccountDTO> basAccountsDTO, IngoingInvoice invoice, int companyId);
+
     }
 }
