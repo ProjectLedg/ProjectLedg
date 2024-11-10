6c4bdf0c7d9fc574990a907d71c1ee64c9e6534e
manhattaa <fadynhatta@gmail.com>
2024-11-10 20:46:10 +0100

    news letter and Targetted Email request

---
+++ ProjectLedg.Server/Options/Newsletter/NewsletterRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace ProjectLedg.Server.Options.Newsletter
+{
+    public class NewsletterRequest
+    {
+        public string Subject { get; set; }
+        public string Content { get; set; }
+    }
+}
