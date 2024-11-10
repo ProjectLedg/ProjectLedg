68ed4f1517462806b7e4d5b98723de259b5d3084
manhattaa <fadynhatta@gmail.com>
2024-11-10 20:46:24 +0100

    NoticeRequest class

---
+++ ProjectLedg.Server/Options/Notice/NoticeRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace ProjectLedg.Server.Options.Notice
+{
+    public class NoticeRequest
+    {
+        public string UserId { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+    }
+}
