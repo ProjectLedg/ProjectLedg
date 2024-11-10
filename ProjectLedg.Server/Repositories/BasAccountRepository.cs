ae1841823b32d1c0e518303444be9952a1c5aabb
manhattaa <fadynhatta@gmail.com>
2024-11-10 20:04:22 +0100

    BasAccountRepository layer

---
+++ ProjectLedg.Server/Repositories/BasAccountRepository.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProjectLedg.Server.Data;
+using ProjectLedg.Server.Repositories.IRepositories;
+
+namespace ProjectLedg.Server.Repositories
+{
+    public class BasAccountRepository : IBasAccountRepository
+    {
+        private readonly ProjectLedgContext _context;
+
+        public BasAccountRepository(ProjectLedgContext context)
+        {
+            _context = context;
+        }
+        public async Task<KeyValuePair<string, int>> GetMostUsedBasAccountAsync()
+        {
+            var mostUsedBasAccount = await _context.Transactions
+                .GroupBy(t => t.BasAccountId)
+                .OrderByDescending(g => g.Count())
+                .Select(g => new KeyValuePair<string, int>(g.Key.HasValue ? g.Key.Value.ToString() : "Unknown", g.Count()))
+                .FirstOrDefaultAsync();
+
+            return mostUsedBasAccount;
+        }
+
+    }
+}
