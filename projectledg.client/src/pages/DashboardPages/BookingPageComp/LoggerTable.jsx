d323dae069aa16f311e58bd2b5d2d522fc7b46df
HjalmarStranninge <hjalmar.stranninge@gmail.com>
2024-11-11 15:08:11 +0100

    Dark mode fix

--- projectledg.client/src/pages/DashboardPages/BookingPageComp/LoggerTable.jsx
+++ projectledg.client/src/pages/DashboardPages/BookingPageComp/LoggerTable.jsx
@@ -36,15 +36,15 @@ const [sortOrder, setSortOrder] = useState("asc") // Default sort order ascendin
 return (
     <Table>
         <TableHeader>
-            <TableRow className="bg-gray-100 dark:bg-black">
-                <DropdownMenu className="flex flex-row justify-around">
-                    <TableHead className="font-bold cursor-pointer" onClick={() => handleSort("id")}>Fakturanummer {sortBy === "id" ? (sortOrder === "asc" ? <ArrowUpNarrowWide className="ml-1 w-4 h-4 inline" /> : <ArrowDownWideNarrow className="ml-1 w-4 h-4 inline" />) : <ListFilter className="ml-1 w-4 h-4 inline" /> }</TableHead>
-                    <TableHead className="font-bold cursor-pointer" onClick={() => handleSort("date")}>Fakturadatum {sortBy === "date" ? (sortOrder === "asc" ? <ArrowUpNarrowWide className="ml-1 w-4 h-4 inline" /> : <ArrowDownWideNarrow className="ml-1 w-4 h-4 inline" />) : <ListFilter className="ml-1 w-4 h-4 inline" />}</TableHead>
-                    <TableHead className="font-bold cursor-pointer" onClick={() => handleSort("dueDate")}>Förfallodatum {sortBy === "dueDate" ? (sortOrder === "asc" ? <ArrowUpNarrowWide className="ml-1 w-4 h-4 inline" /> : <ArrowDownWideNarrow className="ml-1 w-4 h-4 inline" />) : <ListFilter className="ml-1 w-4 h-4 inline" /> }</TableHead>
-                    <TableHead className="font-bold cursor-pointer" onClick={() => handleSort("amount")}>Belopp {sortBy === "amount" ? (sortOrder === "asc" ? <ArrowUpNarrowWide className="ml-1 w-4 h-4 inline" /> : <ArrowDownWideNarrow className="ml-1 w-4 h-4 inline" />) : <ListFilter className="ml-1 w-4 h-4 inline" /> }</TableHead>
+            <TableRow className="bg-gray-100 dark:bg-darkBackground dark:border-darkBorder">
+                <DropdownMenu className="flex flex-row justify-around ">
+                    <TableHead className="font-bold cursor-pointer dark:bg-darkBackground dark:text-darkSecondary" onClick={() => handleSort("id")}>Fakturanummer {sortBy === "id" ? (sortOrder === "asc" ? <ArrowUpNarrowWide className="ml-1 w-4 h-4 inline" /> : <ArrowDownWideNarrow className="ml-1 w-4 h-4 inline" />) : <ListFilter className="ml-1 w-4 h-4 inline" /> }</TableHead>
+                    <TableHead className="font-bold cursor-pointer dark:bg-darkBackground dark:text-darkSecondary" onClick={() => handleSort("date")}>Fakturadatum {sortBy === "date" ? (sortOrder === "asc" ? <ArrowUpNarrowWide className="ml-1 w-4 h-4 inline" /> : <ArrowDownWideNarrow className="ml-1 w-4 h-4 inline" />) : <ListFilter className="ml-1 w-4 h-4 inline" />}</TableHead>
+                    <TableHead className="font-bold cursor-pointer dark:bg-darkBackground dark:text-darkSecondary" onClick={() => handleSort("dueDate")}>Förfallodatum {sortBy === "dueDate" ? (sortOrder === "asc" ? <ArrowUpNarrowWide className="ml-1 w-4 h-4 inline" /> : <ArrowDownWideNarrow className="ml-1 w-4 h-4 inline" />) : <ListFilter className="ml-1 w-4 h-4 inline" /> }</TableHead>
+                    <TableHead className="font-bold cursor-pointer dark:bg-darkBackground dark:text-darkSecondary" onClick={() => handleSort("amount")}>Belopp {sortBy === "amount" ? (sortOrder === "asc" ? <ArrowUpNarrowWide className="ml-1 w-4 h-4 inline" /> : <ArrowDownWideNarrow className="ml-1 w-4 h-4 inline" />) : <ListFilter className="ml-1 w-4 h-4 inline" /> }</TableHead>
                 <DropdownMenuTrigger>
                     <TableHead 
-                        className="font-bold flex justify-center items-center">
+                        className="font-bold flex justify-center items-center dark:text-darkSecondary ">
                             Status 
                             <Filter className="ml-2 w-4 h-4 inline" />
                     </TableHead>
@@ -68,7 +68,7 @@ return (
                 </DropdownMenu>
             </TableRow>
         </TableHeader>
-        <TableBody className="border-b">
+        <TableBody className="border-b dark:border-darkBorder">
             {sortedData
             .filter((item) => item.isPaid === false || item.isPaid === !showUnpaid) // set if only show unpaid items
             .filter((item) => item.isBooked === false || item.isBooked === !showUnbooked) // set if only show unbooked items
@@ -76,7 +76,7 @@ return (
             .map((invoice) => (
                 <TableRow
                     key={invoice.id}
-                    className="cursor-pointer hover:bg-gray-50 transition-colors duration-200"
+                    className="cursor-pointer hover:bg-gray-50 dark:hover:bg-darkBackground dark:border-darkBorder transition-colors duration-200"
                     onClick={() => handleInvoiceClick(invoice)}
                 >
                     <TableCell className="font-medium py-4 px-6">
@@ -93,13 +93,13 @@ return (
                     </TableCell>
                     <TableCell className="w-64">
                         <div className="flex space-x-2">
-                            <Badge className="w-[45%] max-h-5" variant={invoice.isPaid ? "success" : "destructive"}>
+                            <Badge className="w-[45%] max-h-5 dark:text-white" variant={invoice.isPaid ? "success" : "destructive"}>
                                 {invoice.isPaid ? <Check className="h-4 w-4" /> : <X className="h-4 w-4" />}
-                                <span className="ml-1">{invoice.isPaid ? "Betald" : "Ej betald"}</span>
+                                <span className="ml-1 dark:text-white">{invoice.isPaid ? "Betald" : "Ej betald"}</span>
                             </Badge>
-                            <Badge className="w-[45%] max-h-5 " variant={invoice.isBooked ? "success" : "destructive"}>
+                            <Badge className="w-[45%] max-h-5 dark:text-white " variant={invoice.isBooked ? "success" : "destructive"}>
                                 {invoice.isBooked ? <Check className="h-4 w-4" /> : <X className="h-4 w-4" />}
-                                <span className="ml-1">{invoice.isBooked ? "Bokförd" : "Ej Bokförd"}</span>
+                                <span className="ml-1 dark:text-white">{invoice.isBooked ? "Bokförd" : "Ej Bokförd"}</span>
                             </Badge>
                         </div>
                     </TableCell>
