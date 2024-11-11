8c4e09e0da31eea63c0eec63becc834627533c8a
Adrian Rozsahegyi <122897788+Adrozs@users.noreply.github.com>
2024-11-11 15:42:48 +0100

    Renamed repo to repository to keep same name standard

---
+++ ProjectLedg.Server/Repositories/TransactionRepository.cs
@@ -0,0 +1,13 @@
+﻿using ProjectLedg.Server.Repositories.IRepositories;
+using System.Transactions;
+
+namespace ProjectLedg.Server.Repositories
+{
+    public class TransactionRepository : ITransactionRepository
+    {
+        public Task CreateTransactionAsync(Transaction transaction)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
