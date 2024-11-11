4ab91d64ebda1c4b8fe64513a7dc182d1cacb2cb
Adrian Rozsahegyi <122897788+Adrozs@users.noreply.github.com>
2024-11-11 13:35:01 +0100

    Created methods to get and create bas accounts
    -Method to create several bas accounts from a list of basaccounts. Instead of creating 1 on each iteration in our method we save them to a list and create all in 1 database process
    
    -Method to get a bas account by its number for a company
    
    -Method to get all bas accounts for a company (used to verify if bas account exists so we can update it, if it doesnt exist then create a new)

--- ProjectLedg.Server/Repositories/BasAccountRepo.cs
+++ ProjectLedg.Server/Repositories/BasAccountRepo.cs
@@ -1,6 +1,8 @@
-﻿using ProjectLedg.Server.Data;
+﻿using Microsoft.EntityFrameworkCore;
+using ProjectLedg.Server.Data;
 using ProjectLedg.Server.Data.Models;
 using ProjectLedg.Server.Repositories.IRepositories;
+using System.Net.NetworkInformation;
 
 namespace ProjectLedg.Server.Repositories
 {
@@ -16,8 +18,30 @@ namespace ProjectLedg.Server.Repositories
 
         public async Task CreateBasAccountAsync(BasAccount basAccount)
         {
-            await _context.BasAccounts.AddAsync(basAccount);
-            await _context.SaveChangesAsync(); 
+             await _context.BasAccounts.AddAsync(basAccount);
+             await _context.SaveChangesAsync(); 
+        }
+
+        public async Task CreateBasAccountsFromListAsync(List<BasAccount> basAccounts)
+        {
+            // Ensure all bas accounts are added and saved within the local context instance
+            await _context.BasAccounts.AddRangeAsync(basAccounts);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task<BasAccount> GetBasAccountByAccountNumberAsync(string basAccountNumber, int companyId)
+        {
+            return await _context.BasAccounts
+                .Where(ba => ba.CompanyId == companyId && ba.AccountNumber == basAccountNumber)
+                .FirstOrDefaultAsync();
+        }
+
+        public async Task<List<BasAccount>> GetAllBasAccountsByCompanyIdAsync(int companyId)
+        {
+            return await _context.BasAccounts
+                //.AsNoTracking() // Optional, improves performance if no tracking is needed
+                .Where(ba => ba.CompanyId == companyId)
+                .ToListAsync();
         }
     }
 }
