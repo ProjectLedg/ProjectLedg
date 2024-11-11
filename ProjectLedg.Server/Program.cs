0ca388f02e0ae9b2a05b37f37d29c75c4bab3c42
Adrian Rozsahegyi <122897788+Adrozs@users.noreply.github.com>
2024-11-11 15:08:22 +0100

    commented away debug db context scope used to search for errors

--- ProjectLedg.Server/Program.cs
+++ ProjectLedg.Server/Program.cs
@@ -81,9 +81,16 @@ namespace ProjectLedg.Server
 
             services.AddDbContext<ProjectLedgContext>(options =>
             {
-                //options.UseSqlServer(Environment.GetEnvironmentVariable("CONNECTION_STRING"));
-                options.UseSqlServer(Environment.GetEnvironmentVariable("LEDGEDB_CONNECTION_STRING"));
+                options.UseSqlServer(Environment.GetEnvironmentVariable("CONNECTION_STRING"));
+                //options.UseSqlServer(Environment.GetEnvironmentVariable("LEDGEDB_CONNECTION_STRING"));
             });
+
+            //services.AddDbContext<ProjectLedgContext>(options =>
+            //    options.UseSqlServer(Environment.GetEnvironmentVariable("CONNECTION_STRING"),
+            //    sqlOptions => {
+            //        sqlOptions.EnableRetryOnFailure();
+            //        sqlOptions.CommandTimeout(500); // Increase timeout if needed
+            //    }).LogTo(Console.WriteLine, LogLevel.Information));
 
             // Add services to the container.
             services.AddIdentity<User, IdentityRole>(options =>
