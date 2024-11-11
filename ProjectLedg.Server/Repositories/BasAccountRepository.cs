3a9730ef218d4e2de5e75f1b54082ee752becc9b
Adrian Rozsahegyi <122897788+Adrozs@users.noreply.github.com>
2024-11-11 15:48:13 +0100

    added missing methods that disapeared from a merge

--- ProjectLedg.Server/Repositories/BasAccountRepository.cs
+++ ProjectLedg.Server/Repositories/BasAccountRepository.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using ProjectLedg.Server.Data;
+using ProjectLedg.Server.Data.Models;
 using ProjectLedg.Server.Repositories.IRepositories;
 
 namespace ProjectLedg.Server.Repositories
@@ -12,6 +13,7 @@ namespace ProjectLedg.Server.Repositories
         {
             _context = context;
         }
+
         public async Task<KeyValuePair<string, int>> GetMostUsedBasAccountAsync()
         {
             var mostUsedBasAccount = await _context.Transactions
@@ -23,5 +25,31 @@ namespace ProjectLedg.Server.Repositories
             return mostUsedBasAccount;
         }
 
+            public async Task CreateBasAccountAsync(BasAccount basAccount)
+            {
+                await _context.BasAccounts.AddAsync(basAccount);
+                await _context.SaveChangesAsync();
+            }
+            public async Task CreateBasAccountsFromListAsync(List<BasAccount> basAccounts)
+            {
+                // Ensure all bas accounts are added and saved within the local context instance
+                await _context.BasAccounts.AddRangeAsync(basAccounts);
+                await _context.SaveChangesAsync();
+            }
+            public async Task<BasAccount> GetBasAccountByAccountNumberAsync(string basAccountNumber, int companyId)
+            {
+                return await _context.BasAccounts
+                    .Where(ba => ba.CompanyId == companyId && ba.AccountNumber == basAccountNumber)
+                    .FirstOrDefaultAsync();
+            }
+            public async Task<List<BasAccount>> GetAllBasAccountsByCompanyIdAsync(int companyId)
+            {
+                return await _context.BasAccounts
+                    //.AsNoTracking() // Optional, improves performance if no tracking is needed
+                    .Where(ba => ba.CompanyId == companyId)
+                    .ToListAsync();
+            }
+        }
+
     }
-}
+
