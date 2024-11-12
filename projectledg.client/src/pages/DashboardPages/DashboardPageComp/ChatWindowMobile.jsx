c3fa5f8bed1eeb37ab651bb98195fd7e81d245e4
HjalmarStranninge <hjalmar.stranninge@gmail.com>
2024-11-12 13:36:29 +0100

    Added dark mode to mobile chatwindow

--- projectledg.client/src/pages/DashboardPages/DashboardPageComp/ChatWindowMobile.jsx
+++ projectledg.client/src/pages/DashboardPages/DashboardPageComp/ChatWindowMobile.jsx
@@ -93,25 +93,25 @@ export default function ChatWindowMobile({ onClose, onSendMessage }) {
     ));
 
     return (
-        <div className="chatWindow flex flex-col right-0 p-2 w-full h-[100vh] bg-white/60 bg-opacity-80 shadow-lg rounded-2xl">
-            <div className="bg-gray-100 rounded-xl overflow-hidden h-[100%] relative flex flex-col">
+        <div className="chatWindow flex flex-col right-0 p-2 w-full h-[100vh] bg-white/60 dark:bg-darkBackground bg-opacity-80 shadow-lg rounded-2xl">
+            <div className="bg-gray-100 dark:bg-darkSurface rounded-xl overflow-hidden h-[90%] relative flex flex-col">
                 <div className="flex justify-between items-center p-4 shadow-sm">
-                    <button onClick={handleNewChat} className="text-gray-500 hover:text-gray-700 ">
+                    <button onClick={handleNewChat} className="text-gray-500 hover:text-gray-700 dark:text-white ">
                         <SquarePen size={25} />
                     </button>
-                    <button onClick={onClose} className="text-gray-500 bg-gray-200 p-1 rounded-full hover:text-gray-700 ">
-                        <X size={20} />
+                    <button onClick={onClose} className="text-gray-500 bg-gray-200 p-1 rounded-full dark:text-white dark:bg-darkSurface">
+                        <X size={25} />
                     </button>
                 </div>
 
                 {!hasMessages ? (
                     <div className="p-6 space-y-4 flex flex-col justify-around h-[80%]">
                         <div className="space-y-2 flex flex-col items-center text-center">
-                            <h2 className="text-3xl font-normal text-gray-400">
+                            <h2 className="text-3xl font-normal text-gray-400 dark:text-white">
                                 Hej <span className="text-green-500">Placeholder</span>
                             </h2>
-                            <h3 className="text-xl font-semibold text-gray-800">Kan jag hjälpa dig med något?</h3>
-                            <p className="text-md text-gray-600">
+                            <h3 className="text-xl font-semibold text-gray-800 dark:text-white">Kan jag hjälpa dig med något?</h3>
+                            <p className="text-md text-gray-600 dark:text-darkSecondary">
                                 Oavsett om det gäller att hålla koll på utgifter, skapa fakturor eller något annat inom bokföring, finns jag här för dig!
                             </p>
                         </div>
@@ -123,7 +123,7 @@ export default function ChatWindowMobile({ onClose, onSendMessage }) {
                                     className="w-full text-left p-3 h-20 bg-white rounded-lg shadow-sm hover:shadow-md transition-shadow duration-200 flex items-center space-x-2"
                                     onClick={() => handleSuggestionClick(topic)}
                                 >
-                                    <div className="w-8 h-8 text-gray-100 bg-gray-800 rounded-xl flex items-center justify-center">
+                                    <div className="w-8 h-8 text-gray-100 bg-gray-800 rounded-xl flex items-center justify-center dark:bg-darkSurface ">
                                         {index === 0 ? (
                                             <FilePlus className="w-4 h-4" />
                                         ) : index === 1 ? (
@@ -132,7 +132,7 @@ export default function ChatWindowMobile({ onClose, onSendMessage }) {
                                             <FileChartColumn className="w-4 h-4" />
                                         )}
                                     </div>
-                                    <span className="text-sm font-medium text-gray-800">{topic}</span>
+                                    <span className="text-sm font-medium text-gray-800 dark:text-darkSurface">{topic}</span>
                                 </button>
                             ))}
                         </div>
@@ -149,7 +149,7 @@ export default function ChatWindowMobile({ onClose, onSendMessage }) {
                         <div className="flex-grow p-2 h-auto bg-white rounded-3xl flex items-center">
                             <button
                                 type="button"
-                                className="p-2 bg-gray-800 text-white rounded-full hover:bg-gray-500 transition-colors duration-200"
+                                className="p-2 bg-gray-800 text-white rounded-full hover:bg-gray-500 transition-colors duration-200 dark:bg-darkSurface"
                             >
                                 <Paperclip size={20} />
                             </button>
