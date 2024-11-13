7df1dca1477bfecf2a68d2d5f80af1b31854bd75
Fady <145761517+Manhattaa@users.noreply.github.com>
2024-11-13 15:05:24 +0100

    Added the Ledg.se URL to CORS

--- ProjectLedg.Server/Program.cs
+++ ProjectLedg.Server/Program.cs
@@ -156,7 +156,7 @@ namespace ProjectLedg.Server
                 options.AddDefaultPolicy(builder =>
                 {
 
-                    builder.WithOrigins("http://localhost:5173", "https://localhost:5173", "https://accounts.google.com", "https://localhost:7223", "https://localhost:7294", "https://projectledg.azurewebsites.net")
+                    builder.WithOrigins("http://localhost:5173", "https://localhost:5173", "https://accounts.google.com", "https://localhost:7223", "https://localhost:7294", "https://projectledg.azurewebsites.net", "https://ledg.se")
                         .AllowAnyMethod()
                         .AllowAnyHeader()
                         .AllowCredentials()
