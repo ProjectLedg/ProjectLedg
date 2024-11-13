fcf98099460ecb0b17704eedfbcc1574aa30f752
Adrian Rozsahegyi <122897788+Adrozs@users.noreply.github.com>
2024-11-13 18:53:34 +0100

    Changed reset logic to use a reset context instead of passing down properties

--- projectledg.client/src/pages/DashboardPages/BookingPage.jsx
+++ projectledg.client/src/pages/DashboardPages/BookingPage.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react"
+import { ResetProvider } from "@/services/ResetProvider"
 import InvoiceUploader from "./BookingPageComp/InvoiceUploader"
 import InvoicePreview from "./BookingPageComp/InvoicePreview"
 import InvoiceLogger from "./BookingPageComp/InvoiceLogger"
@@ -7,20 +8,18 @@ export default function BookingPage() {
     // State to hold invoice data for child components
     const [invoice, setInvoice] = useState(null); 
     const [isUploadLoading, setIsUploadLoading] = useState(false)
-    const [resetUpFields, setResetUpFields] = useState(false)
-    
-
 
     return (
+        <ResetProvider>
         <div className="container mx-auto   ">
         
             <div className="grid grid-cols-1 gap-6">
                 <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
                     <div className="md:col-span-1">
-                        <InvoiceUploader setInvoice={setInvoice} isUploadLoading={isUploadLoading} setIsUploadLoading={setIsUploadLoading} resetUpFields={resetUpFields}/>
+                        <InvoiceUploader setInvoice={setInvoice} isUploadLoading={isUploadLoading} setIsUploadLoading={setIsUploadLoading} />
                     </div>
                     <div className="md:col-span-2">
-                        <InvoicePreview invoice={invoice} setInvoice={setInvoice} isUploadLoading={isUploadLoading} setIsUploadLoading={setIsUploadLoading} setResetUpFields={setResetUpFields} />
+                        <InvoicePreview invoice={invoice} setInvoice={setInvoice} isUploadLoading={isUploadLoading} setIsUploadLoading={setIsUploadLoading}/>
                     </div>
                     
                 </div>
@@ -29,5 +28,6 @@ export default function BookingPage() {
                 </div>
             </div>
         </div>
+        </ResetProvider>
     )
 }
