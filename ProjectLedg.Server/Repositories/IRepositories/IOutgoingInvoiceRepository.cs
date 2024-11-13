43fc4493aac48e5f2f96f1d909558932579124ec
Adrian Rozsahegyi <122897788+Adrozs@users.noreply.github.com>
2024-11-13 21:47:10 +0100

    -Added logic to retrieve all outgoing invoices for a company

--- ProjectLedg.Server/Repositories/IRepositories/IOutgoingInvoiceRepository.cs
+++ ProjectLedg.Server/Repositories/IRepositories/IOutgoingInvoiceRepository.cs
@@ -5,6 +5,7 @@ namespace ProjectLedg.Server.Repositories.IRepositories
     public interface IOutgoingInvoiceRepository
     {
         Task<OutgoingInvoice?> GetOutgoingInvoiceByIdAsync(int invoiceId);
+        Task<List<OutgoingInvoice>> GetAllOutgoingInvoicesForCompanyAsync(int companyId);
         Task<IEnumerable<OutgoingInvoice>> GetAllOutgoingInvoicesAsync();
         Task<bool> CreateOutgoingInvoiceAsync(OutgoingInvoice invoice);
         Task<bool> UpdateOutgoingInvoiceAsync(OutgoingInvoice invoice);
--- ProjectLedg.Server/Services/IServices/IOutgoingInvoiceService.cs
+++ ProjectLedg.Server/Services/IServices/IOutgoingInvoiceService.cs
@@ -6,6 +6,7 @@ namespace ProjectLedg.Server.Services.IServices
     public interface IOutgoingInvoiceService
     {
         Task<OutgoingInvoice?> GetOutgoingInvoiceByIdAsync(int invoiceId);
+        Task<List<OutgoingInvoice>> GetAllOutgoingInvoicesForCompanyAsync(int companyId);
         Task<IEnumerable<OutgoingInvoice>> GetAllOutgoingInvoicesAsync();
         Task<int> CreateOutgoingInvoiceAsync(OutgoingInvoiceCreationDTO invoiceDto, int companyId);
         Task<bool> UpdateOutgoingInvoiceAsync(int invoiceId, InvoiceDTO invoiceDto);
