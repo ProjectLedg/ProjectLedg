88dd461bc7478af636485812f6425ccdfbc61513
HjalmarStranninge <hjalmar.stranninge@gmail.com>
2024-11-15 13:32:18 +0100

    Removed background in dark mode

--- projectledg.client/src/ChatLoader.jsx
+++ projectledg.client/src/ChatLoader.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function ChatLoader() {
     return (
-        <div className="flex items-center space-x-2 p-4 bg-gray-100 rounded-lg max-w-[100px]">
+        <div className="flex items-center space-x-2 p-4 bg-gray-100 rounded-lg max-w-[100px] dark:bg-darkSurface">
             <div className="w-2 h-2 bg-gray-400 rounded-full animate-bounce"></div>
             <div className="w-2 h-2 bg-gray-400 rounded-full animate-bounce" style={{ animationDelay: '0.2s' }}></div>
             <div className="w-2 h-2 bg-gray-400 rounded-full animate-bounce" style={{ animationDelay: '0.4s' }}></div>
