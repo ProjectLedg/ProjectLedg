f3ba23708286745fbbe479be3d29077704f8e47d
Adrian Rozsahegyi <122897788+Adrozs@users.noreply.github.com>
2024-11-19 11:44:44 +0100

    -Removed image for company select and replaced with avatar to match the user dropdown

--- projectledg.client/src/pages/AccountHandlingPages/CompanySelectPageComponents/CompanyCard.jsx
+++ projectledg.client/src/pages/AccountHandlingPages/CompanySelectPageComponents/CompanyCard.jsx
@@ -1,14 +1,17 @@
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+
+
 export default function CompanyCard({ handleCompanySelect, companyId, companyName, orgNumber, imageUrl }) {
     return (
         <article 
         onClick={() => handleCompanySelect({ companyId, companyName, orgNumber, imageUrl })} 
         className="flex-shrink-0 w-48 h-48 bg-white rounded-lg p-4 shadow-md hover:shadow-lg hover:scale-105 transition-all ease-in-out duration-150 relative group border border-green-100 flex flex-col items-center justify-center">
-            <img 
+            {/* <img 
                 src={imageUrl} 
                 alt={companyName} 
                 className="w-20 h-20 rounded-full mb-2 company-icons object-cover"
                 draggable="false"
-            />
+            /> */}
             <Avatar className={`w-20 h-20 p-[0.125rem] border-2 border-green-500 rounded-full `}>
                 {/* <AvatarImage src={user.avatarUrl} alt={user.name} /> */}
                 <AvatarFallback className="bg-green-50 dark:bg-green-900 text-green-500 text-4xl font-medium">{companyName.charAt(0).toUpperCase()}</AvatarFallback>
