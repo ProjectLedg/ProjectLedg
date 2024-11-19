08e20f3658feb75b443e02e9393a5c2065e138e4
Adrian Rozsahegyi <122897788+Adrozs@users.noreply.github.com>
2024-11-19 11:32:51 +0100

    -Changed compan select card to have letter like in user dropdown to be coherent design

--- projectledg.client/src/pages/AccountHandlingPages/CompanySelectPageComponents/CompanyCard.jsx
+++ projectledg.client/src/pages/AccountHandlingPages/CompanySelectPageComponents/CompanyCard.jsx
@@ -9,7 +9,11 @@ export default function CompanyCard({ handleCompanySelect, companyId, companyNam
                 className="w-20 h-20 rounded-full mb-2 company-icons object-cover"
                 draggable="false"
             />
-            <h3 className="text-lg font-semibold text-center text-green-700 mb-1">{companyName}</h3>
+            <Avatar className={`w-20 h-20 p-[0.125rem] border-2 border-green-500 rounded-full `}>
+                {/* <AvatarImage src={user.avatarUrl} alt={user.name} /> */}
+                <AvatarFallback className="bg-green-50 dark:bg-green-900 text-green-500 text-4xl font-medium">{companyName.charAt(0).toUpperCase()}</AvatarFallback>
+            </Avatar>
+            <h3 className="text-lg font-semibold text-center text-green-600 mb-1">{companyName}</h3>
             <p className="text-sm text-center text-gray-500">{orgNumber}</p>
             <div className="absolute inset-0 bg-green-500 opacity-0 group-hover:opacity-10 rounded-lg transition-opacity duration-150"></div>
         </article>
