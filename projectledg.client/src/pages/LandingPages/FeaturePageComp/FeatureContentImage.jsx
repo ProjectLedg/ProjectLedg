03610a95a6ef0bdf5165f13360d99203064e1ba2
HjalmarStranninge <hjalmar.stranninge@gmail.com>
2024-11-20 18:13:52 +0100

    Added scroll animation and mock chat window

--- projectledg.client/src/pages/LandingPages/FeaturePageComp/FeatureContentImage.jsx
+++ projectledg.client/src/pages/LandingPages/FeaturePageComp/FeatureContentImage.jsx
@@ -1,42 +1,117 @@
-import React from "react";
-import BookingImage from "@/assets/BookingImageFiles.jpg";
-
-const imageText = (
-    <span>
-        Med <span className="text-green-500">Ledge</span> blir ekonomihanteringen smidigare än någonsin. Vi gör det <span className="text-green-500">enkelt</span> för dig att automatisera bokföringen och <span className="text-green-500">snabbt</span> få en överblick över ditt företags ekonomi. Låt oss ta hand om det komplexa, så att du kan fokusera på att utveckla din verksamhet.
-    </span>
-);
-
-
+import React, { useEffect, useState } from "react";
+import ChatWindowDemo from "../FeaturePageComp/ChatWindowDemo";
+import { motion, useScroll, useTransform } from "framer-motion";
+import { Gauge, ChartColumnIncreasing, BotMessageSquare } from "lucide-react";
 
 export default function FeatureImage() {
+    const { scrollYProgress } = useScroll();
+    const [isLocked, setIsLocked] = useState(false);
+
+    useEffect(() => {
+        const unsubscribe = scrollYProgress.onChange((progress) => {
+            if (progress > 0.36 && !isLocked) {
+                console.log("lock")
+                document.body.style.overflow = "hidden";
+
+            } else if (progress <= 0.36 && isLocked) {
+                console.log("unlock")
+                document.body.style.overflow = "auto";
+
+            }
+        });
+
+        return () => {
+            unsubscribe();
+            document.body.style.overflow = "auto";
+        };
+    }, [isLocked]);
+
+    const translateX = useTransform(scrollYProgress, [0, 0.4], [600, -1], {
+        type: "spring",
+        stiffness: 100,
+        damping: 2,
+        mass: 1,
+    });
+
+    const translateXLeft = useTransform(scrollYProgress, [0, 0.4], [-300, 0], {
+        type: "spring",
+        stiffness: 100,
+        damping: 25,
+        mass: 1,
+    });
+
+    const li1Opacity = useTransform(scrollYProgress, [0.4, 0.5], [0, 1]);
+    const li2Opacity = useTransform(scrollYProgress, [0.5, 0.6], [0, 1]);
+    const li3Opacity = useTransform(scrollYProgress, [0.6, 0.7], [0, 1]);
+
     return (
-        <div className="relative w-full flex flex-col items-center overflow-hidden pb-8"> {/* Container with padding-bottom */}
+        <div
+            className={`w-[100vw] flex flex-row items-center overflow-hidden pb-8 ${isLocked ? "bg-red-5§" : "relative"}`}
+        >
             {/* Content container with text and image */}
-            <div className="relative w-full  lg:h-[500px] flex items-center overflow-hidden rounded-2xl shadow-md">
-                {/* Text on the left side with 50% width */}
-                <div className="absolute left-0 z-10 bg-opacity-75 rounded-full text-left w-1/2"
+            <div className="relative w-full flex items-center justify-between overflow-hidden rounded-2xl">
+                {/* Left section */}
+                <motion.div
+                    className="relative h-full flex"
                     style={{
-                        paddingLeft: '4vw',  // 4% of viewport width for smaller screens
-                        paddingRight: '4vw', // 4% of viewport width
+                        translateX: translateXLeft,
                     }}
                 >
-                    <p className="text-gray-700 font-semibold" style={{ fontSize: '2vw' }}>
-                        {imageText}
-                    </p>
-                </div>
+                    <div className="flex flex-col items-start justify-start h-[50vh] w-[60vw] ml-12">
+                        <div className="flex flex-row mb-12">
+                            <h1 className="text-5xl font-medium mr-4">Digital </h1>
+                            <span className="text-5xl font-medium text-green-500">
+                                bokföringsassistent
+                            </span>
+                        </div>
+                        <ul className="space-y-8 text-xl">
+                            <motion.li
+                                className="text-gray-500 flex flex-row items-center"
+                                style={{ opacity: li1Opacity }}
+                            >
+                                <Gauge className="h-8 w-8 mr-2 text-red-500" />
+                                <span className="font-semibold text-black">
+                                    Snabba svar på dina frågor:
+                                </span>
+                                &nbsp;Få klara och tydliga svar om företagets ekonomi och
+                                bokföring.
+                            </motion.li>
+                            <motion.li
+                                className="text-gray-500 flex flex-row items-center"
+                                style={{ opacity: li2Opacity }}
+                            >
+                                <ChartColumnIncreasing className="h-8 w-8 mr-2 text-cyan-500" />
+                                <span className="font-semibold text-black">
+                                    Automatiserade analyser:
+                                </span>
+                                &nbsp;Vår AI hjälper dig att förstå siffrorna och identifiera
+                                viktiga insikter.
+                            </motion.li>
+                            <motion.li
+                                className="text-gray-500 flex flex-row items-center"
+                                style={{ opacity: li3Opacity }}
+                            >
+                                <BotMessageSquare className="h-8 w-8 mr-2 text-green-500" />
+                                <span className="font-semibold text-black">
+                                    Smidig integration:
+                                </span>
+                                &nbsp;Diskutera direkt i vårt chattverktyg för snabbare beslut
+                                och bättre överblick.
+                            </motion.li>
+                        </ul>
+                    </div>
+                </motion.div>
 
-                {/* Image container */}
-                <div className="relative w-full h-full">
-                    <img
-                        src={BookingImage}
-                        alt="Booking Image"
-                        className="w-full h-full object-cover rounded-lg"
-                    />
-                </div>
+                {/* Right section */}
+                <motion.div
+                    className="relative h-full flex"
+                    style={{
+                        translateX,
+                    }}
+                >
+                    <ChatWindowDemo />
+                </motion.div>
             </div>
         </div>
     );
 }
-
-
