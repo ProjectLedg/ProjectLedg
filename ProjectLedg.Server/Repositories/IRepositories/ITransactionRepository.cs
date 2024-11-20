c8f18945d280d387e98c0f70706dbad9319e90ca
Adrian Rozsahegyi <122897788+Adrozs@users.noreply.github.com>
2024-11-20 15:37:15 +0100

    Created interfaces for the transaction repo and service

--- ProjectLedg.Server/Repositories/IRepositories/ITransactionRepository.cs
+++ ProjectLedg.Server/Repositories/IRepositories/ITransactionRepository.cs
@@ -1,9 +1,11 @@
-﻿using System.Transactions;
+﻿
+using ProjectLedg.Server.Data.Models;
 
 namespace ProjectLedg.Server.Repositories.IRepositories
 {
     public interface ITransactionRepository
     {
         Task CreateTransactionAsync(Transaction transaction);
+        Task<List<Transaction>> GetAllTransactionsForCompanyAsync(int companyId);
     }
 }
