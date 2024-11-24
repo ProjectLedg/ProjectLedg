d7e60981fccb7048a00480af39e83f1f03aa7cf9
manhattaa <fadynhatta@gmail.com>
2024-11-24 20:29:39 +0100

    IngoingInvoiceFunctionDTO

---
+++ ProjectLedg.Server/Data/Models/DTOs/Functions/IngoingInvoice/IngoingInvoiceFunctionDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace ProjectLedg.Server.Data.Models.DTOs.Functions.IngoingInvoice
+{
+    public class IngoingInvoiceFunctionDTO
+    {
+        public string InvoiceNumber { get; set; }
+        public string CompanyName { get; set; }
+        public DateTime InvoiceDate { get; set; }
+        public DateTime DueDate { get; set; }
+        public decimal InvoiceTotal { get; set; }
+    }
+}
