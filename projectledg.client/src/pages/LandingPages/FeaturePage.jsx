953f996bf33b3fddadc1858456fe5afbb9cded2b
HjalmarStranninge <hjalmar.stranninge@gmail.com>
2024-11-25 16:15:31 +0100

    Made the navbar hidden when user scrolls to the bottom of the page

--- projectledg.client/src/pages/LandingPages/FeaturePage.jsx
+++ projectledg.client/src/pages/LandingPages/FeaturePage.jsx
@@ -1,27 +1,49 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Navbar from "./LandingPageComp/Navbar";
 import FeatureContentTitle from "./FeaturePageComp/FeatureContentTitle";
 import FeatureContentBody from "./FeaturePageComp/FeatureContentBody";
 import FeatureContentImage from "./FeaturePageComp/FeatureContentImage";
-import Footer from "./LandingPageComp/FooterSection"
+import Footer from "./LandingPageComp/FooterSection";
 
 export default function FeaturePage() {
+    const [showNavbar, setShowNavbar] = useState(true);
+    const [lastScrollY, setLastScrollY] = useState(0);
+
+    useEffect(() => {
+        const handleScroll = () => {
+            const scrollY = window.scrollY;
+            const bottomOffset = document.body.scrollHeight - window.innerHeight;
+
+            if (scrollY >= bottomOffset) {
+                setShowNavbar(false);
+            } else if (scrollY < lastScrollY) {
+                setShowNavbar(true);
+            }
+            setLastScrollY(scrollY);
+        };
+
+        window.addEventListener("scroll", handleScroll);
+        return () => {
+            window.removeEventListener("scroll", handleScroll);
+        };
+    }, [lastScrollY]);
 
     return (
         <>
-            <Navbar />
-            <div className="w-full h-[2500px]  flex flex-col 
-            bg-gradient-to-t from-blue-700/30 after: ">
+            <div
+                className={`fixed top-0 w-full z-50 transition-opacity duration-500 ${showNavbar ? "opacity-100" : "opacity-0"
+                    }`}
+            >
+                <Navbar />
+            </div>
+            <div className="w-full h-[2500px] flex flex-col bg-gradient-to-t from-blue-700/30">
                 <FeatureContentTitle />
-
                 <FeatureContentImage />
             </div>
             <div className="bg-gradient-to-b from-blue-700/30 mb-1 transition-colors duration-1000 ease-in-out">
-
                 <FeatureContentBody />
             </div>
-
             <Footer />
         </>
-    )
+    );
 }
