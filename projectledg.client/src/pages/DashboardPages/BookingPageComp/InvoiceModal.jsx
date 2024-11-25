989fe2e8bc5328ec0ff6b30ca7cb1cea8ed7c8dd
Adrian Rozsahegyi <122897788+Adrozs@users.noreply.github.com>
2024-11-25 13:27:13 +0100

    -Darkmode fixes

--- projectledg.client/src/pages/DashboardPages/BookingPageComp/InvoiceModal.jsx
+++ projectledg.client/src/pages/DashboardPages/BookingPageComp/InvoiceModal.jsx
@@ -167,13 +167,13 @@ export default function InvoiceModal({ basAccounts, invoice, isModalOpen, setIsM
 
 
     return (
-        <Dialog open={isModalOpen} onOpenChange={setIsModalOpen} className="focus:outline-none ">
-            <DialogContent className={` w-auto flex flex-col  min-w-[600px]  min-h-[80vh]`}>
+        <Dialog open={isModalOpen} onOpenChange={setIsModalOpen} className="focus:outline-none">
+            <DialogContent className="w-auto flex flex-col  min-w-[600px]  min-h-[80vh]  dark:bg-darkBackground dark:border-darkBorder focus:outline-none">
                 <DialogHeader className={`${isBooked ? "hidden" : "block"}`} >
                     <DialogTitle className="text-2xl font-bold text-green-600 ">Verifikation</DialogTitle>
                 </DialogHeader>
 
-                <div className={`${isBooked ? " hidden" : "flex"} VerificationContainer flex flex-col justify-between h-[80vh] overflow-hidden `}>
+                <div className={`${isBooked ? " hidden" : "flex"} VerificationContainer flex flex-col justify-between h-[80vh] overflow-hidden`}>
                     <div className="TopRow p-2 h-full flex flex-col border-gray-100">
 
                         {/* Chat section */}
@@ -220,24 +220,24 @@ export default function InvoiceModal({ basAccounts, invoice, isModalOpen, setIsM
                             <div className="Fakturanummer grid grid-cols-4 ">
 
                                 <div className="flex flex-col col-span-3 justify-between space-y-2">
-                                    <p className="font-bold text-lg text-gray-600 mb-1">{invoice.VendorAddressRecipient}</p>
-                                    <p className="text-gray-600 ">Belopp</p>
-                                    <p className="text-gray-500 font-light">Var av moms</p>
-                                    <p className="text-gray-600">Fakturadatum</p>
-                                    <p className="text-gray-600">Förfallodatum</p>
+                                    <p className="font-bold text-lg text-gray-600 dark:text-white mb-1">{invoice.VendorAddressRecipient}</p>
+                                    <p className="text-gray-600 dark:text-white ">Belopp</p>
+                                    <p className="text-gray-500 dark:text-white/90 font-light">Var av moms</p>
+                                    <p className="text-gray-600 dark:text-white">Fakturadatum</p>
+                                    <p className="text-gray-600 dark:text-white">Förfallodatum</p>
                                 </div>
                                 <div className="flex flex-col items-end space-y-2">
-                                    <p className="font-bold text-lg text-gray-600 mb-1">{invoice.InvoiceId}</p>
+                                    <p className="font-bold text-lg text-gray-600 dark:text-white mb-1">{invoice.InvoiceId}</p>
                                     <p className="font-semibold text-green-600">{invoice.InvoiceTotal}kr</p>
-                                    <p className="font-light text-gray-500">{invoice.TotalTax}kr</p>
-                                    <p className="text-gray-600 ">{invoice.InvoiceDate}</p>
-                                    <p className="text-gray-600 ">{invoice.DueDate}</p>
+                                    <p className="font-light text-gray-500 dark:text-white/90">{invoice.TotalTax}kr</p>
+                                    <p className="text-gray-600 dark:text-white ">{invoice.InvoiceDate}</p>
+                                    <p className="text-gray-600 dark:text-white ">{invoice.DueDate}</p>
                                 </div>
                             </div>
 
                             {/* Is paid button */}
                             <div className="flex items-center justify-between">
-                                <Label htmlFor="paid-status" className="font-normal text-base text-gray-600">Betald</Label>
+                                <Label htmlFor="paid-status" className="font-normal text-base text-gray-600 dark:text-white">Betald</Label>
 
                                 <Switch isOn={isOn} setIsOn={setIsOn}
                                     id="paid-status"
@@ -260,18 +260,18 @@ export default function InvoiceModal({ basAccounts, invoice, isModalOpen, setIsM
                                 <div>
                                     <h3 className="font-semibold text-lg mb-2">Bokföringsförslag</h3>
                                     <ScrollArea ref={scrollAreaRef} className="max-h-[40vh] w-full overflow-y-auto  ">
-                                        <Table className="Scroll table overflow-y-auto overflow-x-hidden w-full border">
-                                            <TableHeader className="bg-gray-100 dark:bg-black">
+                                        <Table className="Scroll table overflow-y-auto overflow-x-hidden w-full border dark:border-darkBorder">
+                                            <TableHeader className="bg-gray-100 dark:bg-darkBorder dark:border-darkBorder">
                                                 <TableRow >
-                                                    <TableHead className="text-center">Konto</TableHead>
-                                                    <TableHead className="text-left">Beskrivning</TableHead>
-                                                    <TableHead className="text-center">Debet</TableHead>
-                                                    <TableHead className="text-center">Kredit</TableHead>
+                                                    <TableHead className="text-center dark:text-white/90">Konto</TableHead>
+                                                    <TableHead className="text-left dark:text-white/90">Beskrivning</TableHead>
+                                                    <TableHead className="text-center dark:text-white/90">Debet</TableHead>
+                                                    <TableHead className="text-center dark:text-white/90">Kredit</TableHead>
                                                 </TableRow>
                                             </TableHeader>
                                             <TableBody className="">
                                                 {basAccounts.accounts.map((account, index) => (
-                                                    <TableRow key={index} className="text-center">
+                                                    <TableRow key={index} className="text-center hover:bg-gray-50 dark:hover:bg-darkSurface dark:border-darkBorder">
                                                         <TableCell className="font-medium p-4">{account.BasAccount}</TableCell>
                                                         <TableCell className="text-left p-4">{account.Description}</TableCell>
                                                         <TableCell className="p-4">{account.Debit}</TableCell>
