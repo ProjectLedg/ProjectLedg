00226fed9b87c37a2bfe5fe09feaf46b0503cf75
Adrian Rozsahegyi <122897788+Adrozs@users.noreply.github.com>
2024-11-26 15:16:19 +0100

    -Changed so theres now an empty box when unchecked and a box with a checkmark when chekced

--- projectledg.client/src/components/ui/dropdown-menu.jsx
+++ projectledg.client/src/components/ui/dropdown-menu.jsx
@@ -1,6 +1,6 @@
 import * as React from "react"
 import * as DropdownMenuPrimitive from "@radix-ui/react-dropdown-menu"
-import { Check, ChevronRight, Circle } from "lucide-react"
+import { ChevronRight, Circle, Square, SquareCheck, } from "lucide-react"
 
 import { cn } from "@/lib/utils"
 
@@ -79,10 +79,13 @@ const DropdownMenuCheckboxItem = React.forwardRef(({ className, children, checke
     )}
     checked={checked}
     {...props}>
+    {/* Custom Icon Rendering */}
     <span className="absolute left-2 flex h-3.5 w-3.5 items-center justify-center">
-      <DropdownMenuPrimitive.ItemIndicator>
-        <Check className="h-4 w-4" />
-      </DropdownMenuPrimitive.ItemIndicator>
+      {checked ? (
+        <SquareCheck className="h-4 w-4" /> // Icon when checked is true
+      ) : (
+        <Square className="h-4 w-4" /> // Icon when checked is false
+      )}
     </span>
     {children}
   </DropdownMenuPrimitive.CheckboxItem>
